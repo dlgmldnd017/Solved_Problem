c93cd1827ee507b4e9b6abad8d2194278c207d50
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-06-28 22:32:16 +0900

    [Gold IV] Title: 이분 그래프, Time: 1136 ms, Memory: 303900 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,94 +1,95 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Node{
-	int end, color;
-	
-	public Node(int end, int color) {
-		this.end=end;
-		this.color=color;
-	}
+    int end, color;
+
+    public Node(int end, int color){
+        this.end = end;
+        this.color = color;
+    }
 }
 
 public class Main {
-	static int V, E, ans;
-	static int[] c;
-	static ArrayList<Node>[] arr;
-	static boolean[] visited;
-	
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int test_case = Integer.parseInt(sc.readLine());
-		for(int T=1; T<=test_case; T++) {
-			
-			st = new StringTokenizer(sc.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			
-			arr = new ArrayList[V+1];
-			for(int i=1; i<=V; i++) {
-				arr[i] = new ArrayList<>();
-			}
-			
-			for(int i=0; i<E; i++) {
-				st = new StringTokenizer(sc.readLine());
-				int y = Integer.parseInt(st.nextToken());
-				int x = Integer.parseInt(st.nextToken());
-				
-				arr[y].add(new Node(x, -1));
-				arr[x].add(new Node(y, -1));
-			}
-			
-			ans=0;
-			c = new int[V+1];
-			visited = new boolean[V+1];
-			
-			for(int i=1; i<=V; i++) {
-				if(!visited[i]) solve(i);
-				
-				for(Node n:arr[i]) {
-					if(c[i]==c[n.end]) {
-						ans=1;
-						break;
-					}
-				}
-			}
-			
-			if(ans==1) sb.append("NO\n");
-			else sb.append("YES\n");
-		}
-		System.out.println(sb);
-	}
-	
-	static void solve(int start) {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(start, 1));
-		
-		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			start = cur.end;
-			c[start] = cur.color;
-			
-			int color=-1;
-			if(visited[start]) continue;
-			visited[start]=true;
-			
-			if(cur.color==1) color=0;
-			else color=1;
-			
-			for(Node n : arr[start]) {
-				if(!visited[n.end]) {
-					q.add(new Node(n.end, color));
-				}
-			}
-		}
-	}
+    static StringBuilder sb = new StringBuilder();
+
+    static int V, E;
+    static ArrayList<Node> map[];
+    static int c[];
+    static boolean visited[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int T=1; T<=t; T++) {
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken())+1;
+            E = Integer.parseInt(st.nextToken());
+
+            map = new ArrayList[V];
+
+            for(int i=1; i<V; i++){
+                map[i] = new ArrayList<>();
+            }
+
+            for(int i=0; i<E; i++){
+                st = new StringTokenizer(br.readLine());
+                int y = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+
+                map[y].add(new Node(x, -1));
+                map[x].add(new Node(y, -1));
+            }
+
+            c = new int[V];
+            visited = new boolean[V];
+
+            int ans = 0;
+
+            for(int i=1; i<V; i++){
+                if(!visited[i]) solve(i);
+
+                for(Node node : map[i]){
+                    if(c[i] == c[node.end]){
+                        ans=1;
+                        break;
+                    }
+                }
+
+                if(ans==1) break;
+            }
+
+            if(ans==1) sb.append("NO\n");
+            else sb.append("YES\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void solve(int start){
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(start, 0));
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            c[cur.end] = cur.color;
+
+            if(visited[cur.end]) continue;
+            visited[cur.end] = true;
+
+            int color = -1;
+
+            if(cur.color == 0) color = 1;
+            else color = 0;
+
+            for(Node node : map[cur.end]){
+                if(visited[node.end]) continue;
+
+                q.add(new Node(node.end, color));
+            }
+        }
+    }
 }
