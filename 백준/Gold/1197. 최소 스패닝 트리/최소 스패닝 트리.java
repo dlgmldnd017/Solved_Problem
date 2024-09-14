cf1364b8b7354cd7ce8884ece743c960ffd47b05
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-09-14 14:26:37 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 580 ms, Memory: 57432 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,84 +1,68 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-class Graph implements Comparable<Graph>{
-	int idx, d;
-	
-	public Graph(int idx, int d) {
-		super();
-		this.idx = idx;
-		this.d = d;
-	}
+class Node implements Comparable<Node>{
+    int E, W;
 
-	@Override
-	public int compareTo(Graph o) {
-		return d-o.d;
-	}
+    public Node(int E, int W){
+        this.E = E;
+        this.W = W;
+    }
+
+    public int compareTo(Node n){
+        return this.W - n.W;
+    }
 }
 
-public class Main {
-	static int N, M, ans;
-	static int dist[];
-	
-	static List<Graph> list[];
+public class Main{
+    static int V, E, ans;
+    static List<Node> list[];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		st = new StringTokenizer(sc.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		dist = new int[N];
-		list = new ArrayList[N];
-		
-		for(int i=0; i<N; i++) {
-			dist[i] = Integer.MAX_VALUE;
-			list[i] = new ArrayList<Graph>();
-		}
-		dist[0]=0;
-		
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(sc.readLine());
-			int y = Integer.parseInt(st.nextToken())-1;
-			int x = Integer.parseInt(st.nextToken())-1;
-			int d = Integer.parseInt(st.nextToken());
-			
-			list[y].add(new Graph(x, d));
-			list[x].add(new Graph(y, d));
-		}
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
-		solve();
-		for(int i=1; i<N; i++) {
-			ans += dist[i];
-		}
-		System.out.println(ans);
-	}
+        list = new ArrayList[V+1];
 
-	static void solve() {
-		PriorityQueue<Graph> pq = new PriorityQueue<Graph>();
-		pq.add(new Graph(0, 0));
-		
-		boolean visited[] = new boolean[N];
-		
-		while(!pq.isEmpty()) {
-			Graph cur = pq.poll();
-			
-			if(visited[cur.idx]) continue;
-			visited[cur.idx]=true;
-			
-			for(Graph g : list[cur.idx]) {
-				if(visited[g.idx] || dist[g.idx]<g.d) continue;
-				
-				dist[g.idx] = g.d;
-				pq.add(new Graph(g.idx, dist[g.idx]));
-			}
-		}
-	}
+        for(int i=1; i<=V; i++){
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i=1; i<=E; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            list[A].add(new Node(B,C));
+            list[B].add(new Node(A,C));
+        }
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve(){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(1, 0));
+        
+        boolean visited[] = new boolean[V+1];
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            
+            if(visited[cur.E]) continue;
+            visited[cur.E] = true;
+            ans += cur.W;
+
+            for(Node next : list[cur.E]){
+                pq.add(next);
+            }
+        }
+    }
 }
