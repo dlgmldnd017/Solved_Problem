412149bcff54961744fe26d78463922939359293
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-09-15 15:08:17 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 15960 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,39 +3,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, ans;
-	static int tmp[] = {1, 1, 1};
-	static int map[][];
+    static int N, ans;
+    static int map[][];
 
-	static int cache[];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		N = Integer.parseInt(sc.readLine());
+        N = Integer.parseInt(sc.readLine());
 
-		map = new int[N][N];
+        map = new int[N][N];
 
-		cache = new int[N];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(sc.readLine());
 
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(sc.readLine());
+            for(int j=0; j<3; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-			for(int j=0; j<3; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        solve();
+    }
 
-		solve();
-	}
-
-	static void solve() {
-		for(int i=1; i<N; i++) {
-			map[i][0] += Math.min(map[i-1][1], map[i-1][2]);
-			map[i][1] += Math.min(map[i-1][0], map[i-1][2]);
-			map[i][2] += Math.min(map[i-1][0], map[i-1][1]);
-		}
-		System.out.println(Math.min(map[N-1][0], Math.min(map[N-1][1], map[N-1][2])));
-	}
+    static void solve() {
+        for(int i=1; i<N; i++) {
+            map[i][0] += Math.min(map[i-1][1], map[i-1][2]);
+            map[i][1] += Math.min(map[i-1][0], map[i-1][2]);
+            map[i][2] += Math.min(map[i-1][0], map[i-1][1]);
+        }
+        System.out.println(Math.min(map[N-1][0], Math.min(map[N-1][1], map[N-1][2])));
+    }
 }
