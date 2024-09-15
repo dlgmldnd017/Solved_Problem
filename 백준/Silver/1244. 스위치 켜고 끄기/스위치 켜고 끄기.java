9ce120608b775d070b8420b4b5dcaefe83c7767f
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-09-15 16:31:19 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 64 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,86 +1,83 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	//    각각 스위치 수, 학생 수
-	static int swiN, stuN;
-	//    현재 스위치 상태
-	static int[] stat;
-	//    학생 성별과 받은 수의 상태값
-	static int[][] map;
+public class Main{
+    static int N, K, S[], stu[][];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		swiN = Integer.parseInt(sc.readLine());
-		
-		stat = new int[swiN+1];
-		st = new StringTokenizer(sc.readLine());
-		for(int i=1; i<=swiN; i++) {
-			stat[i] = Integer.parseInt(st.nextToken());
-		}
+        N = Integer.parseInt(br.readLine());
 
-		stuN = Integer.parseInt(sc.readLine());
-		map = new int[stuN][2];
-		for(int i=0; i<stuN; i++) {
-			st = new StringTokenizer(sc.readLine());
-			int XY = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
+        S = new int[N+1];
 
-			map[i][0] = XY;
-			map[i][1] = n;
-		}
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            S[i] = Integer.parseInt(st.nextToken());
+        }
 
-		solve();
-		printStat();
-	}
+        K = Integer.parseInt(br.readLine());
 
-	public static void solve() {
-		for(int i=0; i<stuN; i++) {
-			int XY = map[i][0];
-			int n = map[i][1];
-			
-//			남성일 경우
-			if(XY==1) {
-				for(int j=1; j<=swiN; j++) {
-					if((j)%n==0) {
-						stat[j] = stat[j]==1?0:1;
-					}
-				}
-//			여성일경우
-			}else {
-				int start = -1, end=-1;
-				
-				for(int j=1; j<=swiN; j++) {
-					if(j%n==0) {
-						start = j-1;
-						end = j+1;
-						while(true) {
-							if(!inRange(start, end) || stat[start]!=stat[end]) break;
-							stat[start] = stat[start]==1?0:1;
-							stat[end] = stat[end]==1?0:1;
-							start--; end++;
-						}
-						stat[j] = stat[j]==1?0:1;
-						break;
-					}
-				}
-			}
-		}
-	}
-	
-//	여성일 경우 대칭을 구할 때 범위안에 드는 것인지 확인하는 메서드
-	public static boolean inRange(int j1, int j2) {
-		return (j1>=1)&&(j2<=swiN);
-	}
-	
-//	현재 스위치 상태를 출력하는 메서드
-	public static void printStat() {
-		for(int i=1; i<=swiN; i++) {
-			System.out.print(stat[i] + " ");
-			if(i%20==0) System.out.println();
-		}
-	}
+        stu = new int[K][2];
+
+        for(int i=0; i<K; i++){
+            st = new StringTokenizer(br.readLine());
+
+            for(int j=0; j<2; j++){
+               stu[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solve();
+
+        printS();
+    }
+
+    static void solve(){
+        for(int i=0; i<K; i++){
+            switch(stu[i][0]){
+                case 1:
+                    changeM(stu[i][1]);
+                    break;
+
+                case 2:
+                    changeW(stu[i][1]);
+                    break;
+            }
+        }
+    }
+
+    static void changeM(int idx){
+        for(int j=1; j<=N; j++) {
+            if((j)%idx==0) {
+                S[j] = S[j]==1?0:1;
+            }
+        }
+    }
+
+    static void changeW(int idx){
+        int i = 1;
+
+        S[idx] = S[idx] == 1 ? 0 : 1;
+
+        if(idx == 1 || idx == N || S[idx-i] != S[idx+i]) return;
+
+        do{
+            S[idx-i] = S[idx-i] == 1 ? 0 : 1;
+            S[idx+i] = S[idx+i] == 1 ? 0 : 1;
+            i++;
+        } while(inRange(idx-i, idx+i) && S[idx-i] == S[idx+i]);
+    }
+
+    static boolean inRange(int left, int right){
+        return (left>=1) && (right<=N);
+    }
+
+    static void printS(){
+        for(int i=1; i<=N; i++){
+            System.out.print(S[i] + " ");
+            if(i%20==0) System.out.println();
+        }
+    }
 }
