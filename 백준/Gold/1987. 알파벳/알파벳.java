20aaaffaa839bf17cfff604d18d8b876b4759709
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-09-19 23:51:16 +0900

    [Gold IV] Title: 알파벳, Time: 652 ms, Memory: 122832 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,61 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int R, C, ans;
-	static int tmp[];
-	static char map[][];
-	
-	static int dy[] = {-1, 1, 0, 0};
-	static int dx[] = {0, 0, -1, 1};
+class Node{
+    int y, x, cnt, key;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public Node(int y, int x, int cnt, int key){
+        this.y = y;
+        this.x = x;
+        this.cnt = cnt;
+        this.key = key;
+    }
+}
 
-		st = new StringTokenizer(sc.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		map = new char[R][C];
-		
-		for(int i=0; i<R; i++) {
-			String str = sc.readLine();
-			
-			for(int j=0; j<C; j++) {
-				map[i][j] = str.charAt(j);
-			}
-		}
+public class Main{
+    static int R,C, ans;
+    static char map[][];
 
-		tmp = new int[26];
-		ans = Integer.MIN_VALUE;
-		
-		solve(0, 0, 0);
-		if(ans==Integer.MIN_VALUE) ans=1;
-		System.out.println(ans);
-	}
+    static int dy[] = {0, 0, -1, 1};
+    static int dx[] = {-1, 1, 0, 0};
 
-	static void solve(int y, int x, int cnt) {
-		if(tmp[map[y][x]-'A']>=1) {
-			if(ans<cnt) ans=cnt;
-			return;
-		}
-		
-		tmp[map[y][x]-'A']++;
-		for(int k=0; k<4; k++) {
-			int ny = y+dy[k];
-			int nx = x+dx[k];
-			
-			if(!inRange(ny, nx)) continue;
-			
-			solve(ny, nx, cnt+1);
-		}
-		tmp[map[y][x]-'A']--;
-	}
-	
-	static boolean inRange(int y, int x) {
-		return (y>=0&&y<R) && (x>=0&&x<C);
-	}
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new char[R][C];
+
+        for(int i=0; i<R; i++){
+            String str = br.readLine();
+
+            for(int j=0; j<C; j++){
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve(){
+        int key = 1 << (map[0][0]-'A');
+
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(0, 0, 1, key));
+
+        Set<String> visited = new HashSet<>();
+        visited.add(0+","+0+","+key);
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+
+            ans = Math.max(ans, cur.cnt);
+
+            for(int k=0; k<4; k++){
+                int ny = dy[k] + cur.y;
+                int nx = dx[k] + cur.x;
+
+                if(!inRange(ny, nx) || (cur.key & 1 << (map[ny][nx]-'A')) > 0) continue;
+
+                int newKey = cur.key | (1<<(map[ny][nx]-'A'));
+
+                String state = ny+","+nx+","+newKey;
+                if(visited.contains(state)) continue;
+
+                visited.add(state);
+                q.add(new Node(ny, nx, cur.cnt+1, newKey));
+            }
+        }
+    }
+
+    static boolean inRange(int y, int x){
+        return (y>=0&&y<R) && (x>=0&&x<C);
+    }
 }
