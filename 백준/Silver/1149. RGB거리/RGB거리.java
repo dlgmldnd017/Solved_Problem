1f8b841e4e9b3a8962c8a91eacb912a4938a3501
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-09-29 15:53:48 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12072 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,35 +3,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, ans;
-    static int map[][];
+    static int N, A[][], ans;
 
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(sc.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
+        A = new int[N][3];
 
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(sc.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            for(int j=0; j<3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 3; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        ans = Integer.MAX_VALUE;
+
         solve();
+
+        System.out.println(ans);
     }
 
     static void solve() {
-        for(int i=1; i<N; i++) {
-            map[i][0] += Math.min(map[i-1][1], map[i-1][2]);
-            map[i][1] += Math.min(map[i-1][0], map[i-1][2]);
-            map[i][2] += Math.min(map[i-1][0], map[i-1][1]);
+        for (int i = 1; i < N; i++) {
+            A[i][0] += Math.min(A[i-1][1], A[i-1][2]);
+            A[i][1] += Math.min(A[i-1][0], A[i-1][2]);
+            A[i][2] += Math.min(A[i-1][0], A[i-1][1]);
         }
-        System.out.println(Math.min(map[N-1][0], Math.min(map[N-1][1], map[N-1][2])));
+
+        ans = Math.min(A[N-1][0], Math.min(A[N-1][1], A[N-1][2]));
     }
 }
+
