d7358acf1f399eef81ca64159d46a37be5ba9643
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-10-03 20:24:32 +0900

    [Silver V] Title: 색종이, Time: 72 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -2,33 +2,36 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int N, ans;
-	static boolean[][] map;
+public class Main {
+    static int N, ans;
+    static boolean visited[][];
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		N = Integer.parseInt(sc.readLine());
-		map = new boolean[100][100];
-		
-		int sum=0;
-		for(int k=0; k<N; k++) {
-			st = new StringTokenizer(sc.readLine());
-			int y=Integer.parseInt(st.nextToken());
-			int x=Integer.parseInt(st.nextToken());
-			
-			for(int i=y; i<y+10; i++) {
-				for(int j=x; j<x+10; j++) {
-					if(!map[i][j]) {
-						map[i][j]=true;
-						sum++;
-					}
-				}
-			}
-		}
-		System.out.println(sum);
-	}
+        N = Integer.parseInt(br.readLine());
+
+        visited = new boolean[100][100];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            solve(y, x);
+        }
+
+        System.out.println(ans);
+    }
+
+    static void solve(int y, int x) {
+        for (int i = y; i < y + 10; i++) {
+            for (int j = x; j < x + 10; j++) {
+                if(visited[i][j]) continue;
+                visited[i][j] = true;
+                ans++;
+            }
+        }
+    }
 }
