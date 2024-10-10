0ee6633f2c606a2b618111abec0104f8d7f53c23
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-10-10 15:53:11 +0900

    [Gold V] Title: 개똥벌레, Time: 500 ms, Memory: 34260 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, H, down[], up[], min, cnt;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        down = new int[N / 2];
+        up = new int[N / 2];
+
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) down[i / 2] = Integer.parseInt(br.readLine());
+            else up[i / 2] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(down);
+        Arrays.sort(up);
+
+        min = N;
+
+        solve();
+
+        System.out.println(min + " " + cnt);
+    }
+
+    static void solve() {
+        for (int i = 1; i < H + 1; i++) {
+            int start = getObstaclesCnt(down, i);
+            int end = getObstaclesCnt(up, H - i + 1);
+
+            if (min > start + end) {
+                min = start + end;
+                cnt = 1;
+            } else if (min == start + end) cnt++;
+        }
+    }
+
+    static int getObstaclesCnt(int arr[], int h) {
+        int low = 0, high = N / 2;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= h) high = mid;
+            else low = mid + 1;
+        }
+
+        return arr.length - high;
+    }
+}
