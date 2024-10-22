ac55c8e52a01e0863a2e0c50daee612025424302
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-10-22 16:50:24 +0900

    [Silver III] Title: 주유소, Time: 252 ms, Memory: 31964 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -3,8 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static long city[], stationCost[], ans;
+    static int N, len[], cost[], ans;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,19 +11,15 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        city = new long[N - 1];
-
+        len = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N - 1; i++) {
-            city[i] = Long.parseLong(st.nextToken());
-        }
+        for (int i = 0; i < N - 1; i++) len[i] = Integer.parseInt(st.nextToken());
 
-        stationCost = new long[N];
-
+        cost = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            stationCost[i] = Long.parseLong(st.nextToken());
-        }
+        for (int i = 0; i < N; i++) cost[i] = Integer.parseInt(st.nextToken());
+
+        ans = Integer.MAX_VALUE;
 
         solve();
 
@@ -32,12 +27,12 @@ public class Main {
     }
 
     static void solve() {
-        long min = stationCost[0];
+        ans = cost[0] * len[0];
+        int min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < city.length; i++) {
-            if (stationCost[i] < min) min = stationCost[i];
-
-            ans += (min * city[i]);
+        for (int i = 1; i < N; i++) {
+            min = Math.min(min, Math.min(cost[i - 1], cost[i]));
+            ans += min * len[i];
         }
     }
 }
