1680f0c381d2831e9249e5a9a3b20f4394192013
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-10-23 14:42:36 +0900

    [Silver II] Title: N과 M (12), Time: 96 ms, Memory: 15396 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, arr[];
+    static List<Integer> list = new ArrayList<>();
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        solve(0);
+
+        System.out.println(sb);
+    }
+
+    static void solve(int depth) {
+        if (depth == M) {
+            for (int i : list) sb.append(i + " ");
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = 0; i < N; i++) {
+            if ((list.size() > 0 && list.get(list.size() - 1) > arr[i]) || prev == arr[i]) continue;
+
+            list.add(arr[i]);
+            solve(depth + 1);
+            prev = arr[i];
+            list.remove(list.size() - 1);
+        }
+    }
+}
