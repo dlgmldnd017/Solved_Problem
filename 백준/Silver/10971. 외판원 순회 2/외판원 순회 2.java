38e409a8d362642a8705b5b27043cbbdcb4779ee
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-10-23 19:11:04 +0900

    [Silver II] Title: 외판원 순회 2, Time: 64 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,62 +1,48 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, ans;
-	static int map[][];
+    static int N, dp[][], map[][], ans;
 
-	static ArrayList<Integer> list;
-	static boolean visited[];
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		N = Integer.parseInt(sc.readLine());
-		
-		map = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(sc.readLine());
-			
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		ans = Integer.MAX_VALUE;
-		
-		list = new ArrayList<Integer>();
-		list.add(0);
-		
-		visited = new boolean[N];
-		visited[0]=true;
-		
-		solve(0, 0);
-		
-		System.out.println(ans);
-	}
+        N = Integer.parseInt(br.readLine());
 
-	static void solve(int start,  int length) {
-		if(list.size()==N) {
-			if(map[list.get(list.size()-1)][0]!=0) {
-				ans = Math.min(ans, length+map[list.get(list.size()-1)][0]);
-				return;
-			}
-		}
-		
-		for(int i=1; i<N; i++) {
-			if(!visited[i] && map[start][i]!=0) {
-				visited[i]=true;
-				list.add(i);
-				
-				solve(i, length+map[start][i]);
-				
-				visited[i]=false;
-				list.remove(list.size()-1);
-			}
-		}
-	}
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N][(1 << N) - 1];
+
+        for (int i = 0; i < N; i++) Arrays.fill(dp[i], -1);
+
+        ans = solve(0, 1);
+
+        System.out.println(ans);
+    }
+
+    static int solve(int cur, int visit) {
+        if (visit == (1 << N) - 1) {
+            if (map[cur][0] == 0) return 10_000_000;
+            return map[cur][0];
+        }
+
+        if (dp[cur][visit] != -1) return dp[cur][visit];
+        dp[cur][visit] = 10_000_000;
+
+        for (int i = 1; i < N; i++) {
+            if ((visit & (1 << i)) != 0 || map[cur][i] == 0) continue;
+
+            dp[cur][visit] = Math.min(solve(i, visit | 1 << i) + map[cur][i], dp[cur][visit]);
+        }
+
+        return dp[cur][visit];
+    }
 }
