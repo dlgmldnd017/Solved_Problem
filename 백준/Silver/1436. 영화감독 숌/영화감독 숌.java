006c5ca4784f36ba25abb83ec03c1948f5d45249
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-10-26 00:40:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 328 ms, Memory: 158568 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,77 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	private static BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-	private static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(sc.readLine());
-		solve(N);
-	}
-	
-	public static void solve(int n) {
-		int count = 1;
-		int digit = 0; 
-		int num = 0;	
-		
-		while(true) {
-			if (((digit % 10000) / 10) == 666 && digit % 10 != 6) {
-				for (int i = 0; i < 1000; i++) {
-					if (count == n) {
-						System.out.print(digit * 1000 + num);
-						return;
-					}
-					num++;
-					count++;
-				}
-				digit++;
-			}
-			else if (digit % 1000 == 666) {
-				for (int i = 0; i < 1000; i++) {
-					if (count == n) {
-						System.out.print(digit * 1000 + i);
-						return;
-					}
-					count++;
-					num++;
-				}
-				digit++;
-				
-			}else if (digit % 100 == 66) {
-				num = 600;
-				for (int i = 0; i < 100; i++) {
-					if (count == n) {
-						System.out.print(digit * 1000 + num);
-						return;
-					}
-					count++;
-					num++;
-				}
-				digit++;
-				
-			}else if (digit % 10 == 6) {
-				num = 660;
-				for (int i = 0; i < 10; i++) {
-					if (count == n) {
-						System.out.print(digit * 1000 + num);
-						return;
-					}
-					num++;
-					count++;
-				}
-				digit++;
-				
-			}else {
-				num = 666;
-				if (count == n) {
-					System.out.print(digit * 1000 + num);
-					return;
-				}
-				count++;
-				digit++;
-			}
-		}
-	}
+    static int N, ans;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve() {
+        int num = 666, cnt = 1;
+
+        while (cnt != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) cnt++;
+        }
+
+        ans = num;
+    }
 }
