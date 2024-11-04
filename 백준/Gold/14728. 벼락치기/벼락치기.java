c4dcf0adefce976f9d0e38f784263f904f60ab7c
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-04 10:54:37 +0900

    [Gold V] Title: 벼락치기, Time: 84 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -1,40 +1,44 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, T, arr[][], dp[][], ans;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int n = scan.nextInt();
-        int t = scan.nextInt();
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
 
-        Node[] node = new Node[n + 1];
-        for(int i = 1; i <= n; i++) {
-            int k = scan.nextInt();
-            int s = scan.nextInt();
-            node[i] = new Node(k, s);
+        arr = new int[N + 1][2];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int K = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = K;
+            arr[i][1] = S;
         }
 
-        int[][] dp = new int[n + 1][t + 1];
-        for(int i = 1; i <= n; i++) {
-            for(int j = 0; j <= t; j++) {
-                if(node[i].time <= j){
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - node[i].time] + node[i].score);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        System.out.println(dp[n][t]);
+        dp = new int[N + 1][T + 1];
+
+        solve();
+
+        System.out.println(ans);
     }
 
-    public static class Node {
-        int time;
-        int score;
-
-        public Node(int time, int score) {
-            this.time = time;
-            this.score = score;
+    static void solve() {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= T; j++) {
+                if (j >= arr[i][0]) dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - arr[i][0]] + arr[i][1]);
+                else dp[i][j] = dp[i - 1][j];
+            }
         }
+
+        ans = dp[N][T];
     }
 }
