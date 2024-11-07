19ef2a8ef870af49d06cbe09ba560863d17d66c5
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-07 23:43:24 +0900

    [level 2] Title: 구명보트, Time: 11.76 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,19 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);  // 몸무게 오름차순 정렬
-
-        int i = 0;             // 가장 가벼운 사람의 인덱스
-        int j = people.length - 1;  // 가장 무거운 사람의 인덱스
-
-        while (i <= j) {       // 두 인덱스가 만나거나 교차할 때까지 반복
-            if (people[i] + people[j] <= limit) {  // 가장 가벼운 사람과 가장 무거운 사람의 합이 limit 이하면
-                i++;           // 두 사람을 같은 보트에 태움
+        
+        Arrays.sort(people);
+        
+        int low = 0, high = people.length-1;
+        
+        while(low <= high) {
+            int sum = people[low] + people[high];
+            
+            if(sum <= limit) {
+                low++;
+                high--;
+            } else {
+                high--;
             }
-            j--;               // 무거운 사람은 항상 보트에 태움
-            answer++;          // 보트 하나 추가
+            
+            answer++;
         }
-
+        
         return answer;
     }
 }
