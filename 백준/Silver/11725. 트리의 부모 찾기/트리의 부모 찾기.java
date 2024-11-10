8fd125c03d1b273b6263b4643de0b6667d77ffd4
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-10 21:04:06 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 568 ms, Memory: 78584 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,59 +3,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, parent[];
+    static int N, ans[];
     static List<Integer> list[];
+    static boolean visited[];
 
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
 
-        parent = new int[N + 1];
-
         list = new ArrayList[N + 1];
 
-        for (int i = 0; i < N + 1; i++) {
-            list[i] = new ArrayList<>();
-        }
+        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
 
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
-            list[a].add(b);
-            list[b].add(a);
+            list[s].add(e);
+            list[e].add(s);
         }
 
-        solve(1);
+        ans = new int[N + 1];
 
-        for (int i = 2; i < N + 1; i++) sb.append(parent[i] + "\n");
+        visited = new boolean[N + 1];
+
+        solve();
 
         System.out.println(sb);
     }
 
-    static void solve(int start) {
-        boolean visited[] = new boolean[N + 1];
-        visited[start] = true;
-
+    static void solve() {
         Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
+        q.add(1);
 
         while (!q.isEmpty()) {
             int cur = q.poll();
 
+            if (visited[cur]) continue;
+            visited[cur] = true;
+
             for (int next : list[cur]) {
                 if (visited[next]) continue;
 
+                ans[next] = cur;
                 q.add(next);
-                visited[next] = true;
-
-                parent[next] = cur;
             }
         }
+
+        for (int i = 2; i <= N; i++) sb.append(ans[i]+"\n");
     }
 }
