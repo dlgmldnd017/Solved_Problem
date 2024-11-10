ddeefac1e27b652bb58e31f0983807ecfcdbe1f4
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-10 21:59:53 +0900

    [Gold V] Title: 문자열 게임 2, Time: 344 ms, Memory: 20136 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,70 +1,60 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int T, K, min, max;
-    static String str;
+    static int T, K, alpha[];
+    static String W;
 
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         T = Integer.parseInt(br.readLine());
+
         for (int t = 0; t < T; t++) {
-            str = br.readLine();
+            W = br.readLine();
             K = Integer.parseInt(br.readLine());
 
-            if (K == 1) sb.append("1 1");
-            else solve();
+            alpha = new int[26];
 
-            sb.append("\n");
+            for (int i = 0; i < W.length(); i++) alpha[W.charAt(i) - 'a']++;
+
+            solve();
         }
 
         System.out.println(sb);
     }
 
     static void solve() {
-        int i = 0;
+        int min = 10001, max = 1;
 
-        min = Integer.MAX_VALUE;
-        while (i < str.length() - K) {
-            int cnt = 1, idx = -1, j = i + 1;
+        for (int i = 0; i < W.length(); i++) {
+            if (alpha[W.charAt(i) - 'a'] < K) continue;
+
+            int start = i + 1, cnt = 1;
 
             while (cnt != K) {
-                idx = str.indexOf(str.charAt(i), j);
-                if (idx == -1) break;
-                j = idx + 1;
+                start = W.indexOf(W.charAt(i), start);
+
+                if (start == -1) break;
+                start++;
                 cnt++;
             }
 
-            if (cnt == K) if (min > (idx - i + 1)) min = idx - i + 1;
-            i++;
+            if (start == -1) continue;
+            int len = start - i;
+
+            if (min > len) min = len;
+            if (max < len) max = len;
         }
 
-        if (min == Integer.MAX_VALUE) {
-            sb.append("-1");
+        if (min == 10001) {
+            sb.append("-1\n");
             return;
         }
 
-        sb.append(min + " ");
-
-        i = 0;
-
-        max = Integer.MIN_VALUE;
-        while (i < str.length() - K) {
-            int cnt = 1, idx = -1, j = i + 1;
-
-            while (cnt != K) {
-                idx = str.indexOf(str.charAt(i), j);
-                if (idx == -1) break;
-                j = idx + 1;
-                cnt++;
-            }
-
-            if (cnt == K) if (max < (idx - i + 1)) max = idx - i + 1;
-            i++;
-        }
-        sb.append(max);
+        sb.append(min + " " + max + "\n");
     }
 }
