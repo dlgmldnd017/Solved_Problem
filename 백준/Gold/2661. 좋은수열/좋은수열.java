4931afc83e68c071acc781af46dcdb20744a81f1
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-12 10:33:40 +0900

    [Gold IV] Title: 좋은수열, Time: 72 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        solve("");
+    }
+
+    static void solve(String str) {
+        if (str.length() == N) {
+            System.out.println(str);
+            System.exit(0);
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            if (check(str + i)) solve(str + i);
+        }
+    }
+
+    static boolean check(String str) {
+        for (int i = 1; i <= str.length() / 2; i++) {
+            String front = str.substring(str.length() - i * 2, str.length() - i);
+            String back = str.substring(str.length() - i, str.length());
+
+            if (front.equals(back)) return false;
+        }
+
+        return true;
+    }
+}
