cea7271d36414193004dd9fbb318261754de17d7
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-14 11:04:08 +0900

    [Gold V] Title: 흙길 보수하기, Time: 536 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -1,28 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Pool implements Comparable<Pool> {
-    int start, end;
+class Node implements Comparable<Node> {
+    int s, e;
 
-    Pool(int start, int end) {
-        this.start = start;
-        this.end = end;
+    Node(int s, int e) {
+        this.s = s;
+        this.e = e;
     }
 
-    public int compareTo(Pool p) {
-        return this.start - p.start;
+    public int compareTo(Node n) {
+        return this.s - n.s;
     }
 }
 
 public class Main {
     static int N, L, ans;
-    static List<Pool> list = new ArrayList<>();
+    static List<Node> list = new ArrayList<>();
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -32,10 +29,10 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken()) - 1;
 
-            list.add(new Pool(start, end));
+            list.add(new Node(s, e));
         }
 
         Collections.sort(list);
@@ -48,24 +45,15 @@ public class Main {
     static void solve() {
         int prev = -1;
 
-        for (Pool p : list) {
-            int start = p.start;
-            int end = p.end;
+        for (Node cur : list) {
+            if (cur.s < prev) cur.s = prev;
 
-            if (start < prev) {
-                if (prev >= end) continue;
-                start = prev;
+            while (cur.s <= cur.e) {
+                cur.s += L;
+                ans++;
             }
 
-            int diff = end - start;
-
-            if (diff % L == 0) {
-                ans += diff / L;
-                prev = end;
-            } else {
-                ans += diff / L + 1;
-                prev = L * (diff / L) + L + start;
-            }
+            prev = cur.s;
         }
     }
 }
