057f4b8d4df566a2fcc3c7b2441d53100ef147c0
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-18 10:12:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 17304 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,54 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Node{
-    int pos, ans;
+    int x, cnt;
 
-    public Node(int pos, int ans) {
-        super();
-        this.pos = pos;
-        this.ans = ans;
+    Node(int x, int cnt) {
+        this.x = x;
+        this.cnt = cnt;
     }
 }
 
 public class Main {
-    static int N, K, ans=Integer.MAX_VALUE;
+    static int N, K, ans;
+    static boolean visited[];
 
     public static void main(String[] args) throws Exception {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(sc.readLine());
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        solve();
+        ans = Integer.MAX_VALUE;
+
+        if (N < K) solve();
+        else ans = N - K;
+
         System.out.println(ans);
     }
 
     static void solve() {
-        Queue<Node> q = new LinkedList<>();
-        boolean[] visited = new boolean[100_001];
-        q.add(new Node(N, 0));
+        Queue<Node> pq = new ArrayDeque<>();
+        pq.add(new Node(N, 0));
 
-        while(!q.isEmpty()) {
-            Node cur = q.poll();
-            int pos = cur.pos;
+        visited = new boolean[100_001];
 
-            if(pos == K) {
-                ans = cur.ans;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (cur.x == K) {
+                ans = cur.cnt;
                 return;
             }
 
-            if(visited[pos]) continue;
-            visited[pos]=true;
+            if (visited[cur.x]) continue;
+            visited[cur.x] = true;
 
-            if(pos-1>=0) q.add(new Node(pos-1, cur.ans+1));
-            if(pos+1<=100000) q.add(new Node(pos+1, cur.ans+1));
-            if(pos*2<=100000) q.add(new Node(pos*2, cur.ans+1));
+            if (cur.x - 1 >= 0) pq.add(new Node(cur.x - 1, cur.cnt + 1));
+            if (cur.x + 1 <= 100_000) pq.add(new Node(cur.x + 1, cur.cnt + 1));
+            if (cur.x * 2 <= 100_000) pq.add(new Node(cur.x * 2, cur.cnt + 1));
         }
     }
 }
