1e6b079a60e715aa0acbe0400d75964af0c3e6e7
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-23 22:53:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.66 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,50 @@
 import java.util.*;
 
+class Node {
+    int y, x, cnt;
+    
+    Node (int y, int x, int cnt) {
+        this.y = y;
+        this.x = x;
+        this.cnt = cnt;
+    }
+}
+
 class Solution {
-    static int N, M, ans;
+    static int N, M;
+    
     static int dy[] = {0, 0, -1, 1};
     static int dx[] = {-1, 1, 0, 0};
     
     public int solution(int[][] maps) {
-        N = maps.length; M = maps[0].length;
-        ans = -1;
+        N = maps.length;
+        M = maps[0].length;
         
-        solve(maps);
-        return ans;
-    }
-    
-    static void solve(int [][] maps){
-        Queue<Node> q = new LinkedList<>();
+        Queue<Node> q = new ArrayDeque<>();
         q.add(new Node(0, 0, 1));
-        
+    
         boolean visited[][] = new boolean[N][M];
-        visited[0][0]=true;
+        visited[0][0] = true;
         
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Node cur = q.poll();
             
-            for(int k=0; k<4; k++){
-                int ny = cur.y+dy[k];
-                int nx = cur.x+dx[k];
+            for (int k = 0; k < 4; k++) {
+                int ny = dy[k] + cur.y;
+                int nx = dx[k] + cur.x;
                 
-                if(!inRange(ny, nx) || visited[ny][nx] || maps[ny][nx]==0) continue;
+                if (ny == N-1 && nx == M-1) return cur.cnt + 1;
                 
-                if(ny==N-1 && nx==M-1){
-                    ans = cur.cnt+1;
-                    return;
-                }
+                if (!inRange(ny,nx) || visited[ny][nx] || maps[ny][nx] == 0) continue;
                 
-                q.add(new Node(ny, nx, cur.cnt+1));
-                visited[ny][nx]=true;
+                q.add(new Node(ny, nx, cur.cnt + 1));
+                visited[ny][nx] = true;
             }
         }
+        return -1;
     }
     
-    static boolean inRange(int y, int x){
-        return (y>=0&&y<N) && (x>=0&&x<M);
-    }
-}
-
-class Node{
-    int y, x, cnt;
-    
-    public Node(int y, int x, int cnt){
-        this.y = y;
-        this.x = x;
-        this.cnt = cnt;
+    static boolean inRange(int y, int x) {
+        return (y >= 0 && y < N) && (x >= 0 && x < M);
     }
 }
