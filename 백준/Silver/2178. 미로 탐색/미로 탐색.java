7a6f77131604b84036968b63ef8780549014565c
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-11-29 15:24:05 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 12112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,77 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-class Node{
-	int y, x, ans;
-
-	public Node(int y, int x, int ans) {
-		this.y=y;
-		this.x=x;
-		this.ans = ans;
-	}
-}
+import java.util.*;
 
 public class Main {
-	static int N, M, ans;
-	static int[][] map;
-	
-	static boolean[][] visited;
-	
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, 1, 0, -1};
+    static int N, M, ans;
+    static char ch[][];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(sc.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		for(int i=0; i<N; i++) {
-			String str = sc.readLine();
-			
-			for(int j=0; j<M; j++) {
-				map[i][j] = str.charAt(j)-'0';
-			}
-		}
+    static int dy[] = {0, 0, -1, 1};
+    static int dx[] = {-1, 1, 0, 0};
 
-		solve();
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	static void solve() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0, 0, 1));
-		visited = new boolean[N][M];
-		
-		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			int y = cur.y;
-			int x = cur.x;
-			
-			if((y==N-1)&&(x==M-1)) {
-				ans = cur.ans;
-				return;
-			}
-			
-			for(int k=0; k<4; k++) {
-				int ny = y+dy[k];
-				int nx = x+dx[k];
-				
-				if(!inRange(ny, nx) || map[ny][nx]==0 || visited[ny][nx]) continue;
-				
-				visited[ny][nx]=true;
-				q.add(new Node(ny, nx, cur.ans+1));
-			}
-		}
-	}
-	
-	static boolean inRange(int y, int x) {
-		return (y>=0&&y<N) && (x>=0&&x<M);
-	}
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ch = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) ch[i][j] = input.charAt(j);
+        }
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve() {
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(0, 0, 1));
+
+        boolean visited[][] = new boolean[N][M];
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int ny = dy[k] + cur.y;
+                int nx = dx[k] + cur.x;
+
+                if (!inRange(ny, nx) || visited[ny][nx] || ch[ny][nx] == '0') continue;
+
+                if (ny == N - 1 && nx == M - 1) {
+                    ans = cur.cnt + 1;
+                    return;
+                }
+
+                visited[ny][nx] = true;
+                q.add(new Node(ny, nx, cur.cnt + 1));
+            }
+        }
+    }
+
+    static boolean inRange(int y, int x) {
+        return (y >= 0 && y < N) && (x >= 0 && x < M);
+    }
+}
+
+class Node {
+    int y, x, cnt;
+
+    Node(int y, int x, int cnt) {
+        this.y = y;
+        this.x = x;
+        this.cnt = cnt;
+    }
 }
