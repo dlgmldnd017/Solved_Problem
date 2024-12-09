69220575ae94d0bafb313840dbe3852e78307f95
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-12-09 17:13:56 +0900

    [Gold III] Title: 아기 상어, Time: 152 ms, Memory: 21076 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,114 +1,121 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-class Fish implements Comparable<Fish>{
-	int y, x, size, eatCnt, d;
-
-	public Fish(int y, int x, int size, int eatCnt, int d) {
-		this.y = y;
-		this.x = x;
-		this.size = size;
-		this.eatCnt = eatCnt;
-		this.d = d;
-	}
-
-	@Override
-	public int compareTo(Fish o) {
-		if(d==o.d) {
-			if(y==o.y) {
-				return x-o.x;
-			}else {
-				return y-o.y;
-			}
-		}else {
-			return d-o.d;
-		}
-	}
-}
+import java.util.*;
 
 public class Main {
-	static int N, ans;
-	static int map[][];
-	
-	static Queue<Fish> q = new LinkedList<Fish>();
-	static PriorityQueue<Fish> fish = new PriorityQueue<Fish>();
-	
-	static int dy[] = {-1, 0, 1, 0};
-	static int dx[] = {0, 1, 0, -1};
+    static int N, arr[][], cnt, ans;
+    static Shark shark;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    static int dy[] = {0, 0, -1, 1};
+    static int dx[] = {-1, 1, 0, 0};
 
-		N = Integer.parseInt(sc.readLine());
-		map = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(sc.readLine());
-			
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				
-				if(map[i][j]==9) {
-					q.add(new Fish(i, j, 2, 0, 0));
-					map[i][j]=0;
-				}
-			}
-		}
-		
-		solve();
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	static void solve() {
-		while(true) {
-			boolean visited[][] = new boolean[N][N];
-			visited[q.peek().y][q.peek().x]=true;
-			
-			while(!q.isEmpty()) {
-				Fish cur = q.poll();
-				
-				for(int k=0; k<4; k++) {
-					int ny = cur.y+dy[k];
-					int nx = cur.x+dx[k];
-					
-					if(!inRange(ny, nx) || visited[ny][nx] || !canGo(ny, nx, cur.size)) continue;
-					
-					if(map[ny][nx]!=0 && canEat(ny, nx, cur.size)) fish.add(new Fish(ny, nx, cur.size, cur.eatCnt+1, cur.d+1));
-					
-					q.add(new Fish(ny, nx, cur.size, cur.eatCnt, cur.d+1));
-					visited[ny][nx]=true;
-				}
-			}
-			
-			if(fish.isEmpty()) return;
-			
-			Fish curFish = fish.poll();
-			if(curFish.eatCnt==curFish.size) {
-				curFish.size++;
-				curFish.eatCnt=0;
-			}
-			
-			map[curFish.y][curFish.x]=0;
-			q.add(new Fish(curFish.y, curFish.x, curFish.size, curFish.eatCnt, 0));
-			fish.clear();
-			ans += curFish.d;
-		}
-	}
-	
-	static boolean canEat(int ny, int nx, int size) {
-		return map[ny][nx]<size;
-	}
+        N = Integer.parseInt(br.readLine());
 
-	static boolean canGo(int ny, int nx, int size) {
-		return map[ny][nx]<=size;
-	}
+        arr = new int[N][N];
 
-	static boolean inRange(int y, int x) {
-		return (y>=0&&y<N) && (x>=0&&x<N);
-	}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+                if (arr[i][j] == 0) continue;
+                else if (arr[i][j] == 9) shark = new Shark(i, j, 2, 0);
+                else cnt++;
+            }
+        }
+
+        if (cnt != 0) solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve() {
+        arr[shark.y][shark.x] = 0;
+
+        while (true) {
+            Queue<Shark> q = new ArrayDeque<>();
+            q.add(new Shark(shark.y, shark.x, 0));
+
+            boolean visited[][] = new boolean[N][N];
+            visited[shark.y][shark.x] = true;
+
+            List<Fish> fish = new ArrayList<>();
+
+            while (!q.isEmpty()) {
+                Shark cur = q.poll();
+
+                for (int k = 0; k < 4; k++) {
+                    int ny = dy[k] + cur.y;
+                    int nx = dx[k] + cur.x;
+
+                    if (!inRange(ny, nx) || visited[ny][nx] || shark.size < arr[ny][nx]) continue;
+
+                    if (arr[ny][nx] != 0 && shark.size > arr[ny][nx]) fish.add(new Fish(ny, nx, cur.cnt + 1));
+                    visited[ny][nx] = true;
+                    q.add(new Shark(ny, nx, cur.cnt + 1));
+                }
+            }
+
+            if (fish.isEmpty()) return;
+
+            Collections.sort(fish);
+
+            Fish f = fish.get(0);
+            shark.y = f.y;
+            shark.x = f.x;
+            shark.cnt++;
+
+            if (shark.size == shark.cnt) {
+                shark.size++;
+                shark.cnt = 0;
+            }
+
+            arr[f.y][f.x] = 0;
+
+            ans += f.dist;
+        }
+    }
+
+    static boolean inRange(int y, int x) {
+        return (y >= 0 && y < N) && (x >= 0 && x < N);
+    }
+}
+
+class Fish implements Comparable<Fish> {
+    int y, x, dist;
+
+    Fish(int y, int x, int dist) {
+        this.y = y;
+        this.x = x;
+        this.dist = dist;
+    }
+
+    public int compareTo(Fish f) {
+        if (this.dist == f.dist) {
+            if (this.y == f.y) return this.x - f.x;
+            return this.y - f.y;
+        }
+        return this.dist - f.dist;
+    }
+}
+
+class Shark {
+    int y, x, size, cnt;
+
+    Shark(int y, int x, int size, int cnt) {
+        this.y = y;
+        this.x = x;
+        this.size = size;
+        this.cnt = cnt;
+    }
+
+    Shark(int y, int x, int cnt) {
+        this.y = y;
+        this.x = x;
+        this.cnt = cnt;
+    }
 }
