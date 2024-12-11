29c76b74688b4837db4b7d755a6db0aadb4e820b
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-12-11 16:01:27 +0900

    [Gold IV] Title: 합이 0, Time: 336 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, arr[];
+    static long ans;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve() {
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > 0) break;
+
+            int left = i + 1, right = N - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    int l = 1;
+                    int r = 1;
+
+                    if (arr[left] == arr[right]) {
+                        int n = right - left + 1;
+                        ans += getComb(n);
+                        break;
+                    }
+
+                    while (arr[left] == arr[left + 1]) {
+                        l++;
+                        left++;
+                    }
+
+                    while (arr[right] == arr[right - 1]) {
+                        r++;
+                        right--;
+                    }
+
+                    ans += l * r;
+                }
+
+                if (sum > 0) right--;
+                else left++;
+            }
+        }
+    }
+
+    static long getComb(int n) {
+        return n * (n - 1) / 2;
+    }
+}
