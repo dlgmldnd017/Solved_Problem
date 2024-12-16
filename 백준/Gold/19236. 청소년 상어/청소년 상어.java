6959407ea22f3456c0ccdb3c52502f6a1f526f5b
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-12-16 23:30:18 +0900

    [Gold II] Title: 청소년 상어, Time: 64 ms, Memory: 11676 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -1,128 +1,121 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
-class Fish{
-	int y, x, dir;
-	boolean death;
-	public Fish(int y, int x, int dir, boolean death) {
-		this.y = y;
-		this.x = x;
-		this.dir = dir;
-		this.death = death;
-	}
-}
+import java.util.*;
 
 public class Main {
-	static int N=4, ans;
-	static int map[][];
-	
-	static Map<Integer, Fish> dir;
-	
-	static int dy[] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
-	static int dx[] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
+    static int arr[][], ans;
+    static Fish fish[] = new Fish[17];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		map = new int[N][N];
-		dir = new HashMap<Integer, Fish>();
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(sc.readLine());
-			
-			for(int j=0; j<N; j++) {
-				int fishNum = Integer.parseInt(st.nextToken());
-				int dirNum = Integer.parseInt(st.nextToken());
-				
-				map[i][j] = fishNum;
-				dir.put(fishNum, new Fish(i, j, dirNum, false));
-			}
-		}
+    static int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int dx[] = {0, -1, -1, -1, 0, 1, 1, 1};
 
-		solve(0, 0, 0, new HashMap<Integer, Fish>(dir), map);
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	static void solve(int y, int x, int sum, Map<Integer, Fish> copy1, int[][] copy2) {
-		if(!inRange(y, x) || map[y][x]==0) {
-			ans = Math.max(ans, sum);
-			return;
-		}
-		
-		int fishNum = map[y][x];
-		int fishDir = dir.get(fishNum).dir;
-		
-		map[y][x]=0;
-		dir.get(fishNum).death=true;
-		sum += fishNum;
-		
-		moveFish(y, x);
-		
-		copy1 = new HashMap<Integer, Fish>(dir);
-		copy2 = copyArr(map);
-		
-		for(int i=1; i<=N; i++) {
-			int ny = y+dy[fishDir]*i;
-			int nx = x+dx[fishDir]*i;
-			
-			solve(ny, nx, sum, dir, map);
-			dir = new HashMap<Integer, Fish>(copy1);
-			map = copyArr(copy2);
-		}
-		dir.get(fishNum).death=false;
-	}
-	
-	static int[][] copyArr(int[][] original) {
-		int tmp[][] = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			tmp[i] = original[i].clone();
-		}
-		return tmp;
-	}
+        arr = new int[4][4];
 
-	public static void moveFish(int r, int c) {
-        for(int i=1; i<=16; i++) {
-            if(dir.get(i).death) continue;
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            // 현재 물고기 위치 받아오기
-            int num = dir.get(i).dir;
-            int y = dir.get(i).y;
-            int x = dir.get(i).x;
+            for (int j = 0; j < 4; j++) {
+                int idx = Integer.parseInt(st.nextToken());
+                int dir = Integer.parseInt(st.nextToken());
 
-            // 현재 위치에서 반시계로 돌기
-            for(int k=1; k<=8; k++, num++) {
-                if(num==9) num=1;
-
-                int ny = y+dy[num];
-                int nx = x+dx[num];
-
-                // 범위를 벗어나거나 상어 위치라면 못가니깐 다시 다른 방향 탐색
-                if(!inRange(ny, nx) || (ny==r&&nx==c)) continue;
-                
-                // 바라보는 방향에 물고기가 있다면 위치 변경하기
-                if(map[ny][nx]!=0) {
-                    int key = map[ny][nx];
-                    map[ny][nx] = map[y][x];
-                    map[y][x] = key;
-                    dir.put(i, new Fish(ny, nx, num, false));
-                    dir.put(key, new Fish(y, x, dir.get(key).dir, false));
-                    break;
-                }
-
-                map[ny][nx] = map[y][x];
-                map[y][x] = 0;
-                dir.put(i, new Fish(ny, nx, num, false));
-                break;
+                arr[i][j] = idx;
+                fish[idx] = new Fish(i, j, dir - 1, 1);
             }
         }
+
+        solve(0, 0, fish[arr[0][0]].dir, arr[0][0]);
+
+        System.out.println(ans);
     }
-	
-	static boolean inRange(int y, int x) {
-		return (y>=0&&y<N) && (x>=0&&x<N);
-	}
+
+    static void solve(int y, int x, int dir, int cnt) {
+        if (ans < cnt) ans = cnt;
+
+        // 현재 상태 저장
+        int[][] tempArr = copyArray(arr);
+        Fish[] tempFish = copyFish(fish);
+
+        fish[arr[y][x]].alive = 0;
+
+        moveFish(y, x);
+
+        for (int k = 1; k <= 3; k++) {
+            int ny = dy[dir] * k + y;
+            int nx = dx[dir] * k + x;
+
+            if (!inRange(ny, nx) || fish[arr[ny][nx]].alive == 0) continue;
+
+            solve(ny, nx, fish[arr[ny][nx]].dir, cnt + arr[ny][nx]);
+        }
+
+        // 이전 상태 복구
+        arr = tempArr;
+        fish = tempFish;
+    }
+
+    static int[][] copyArray(int[][] src) {
+        int[][] copy = new int[4][4];
+        for (int i = 0; i < src.length; i++) {
+            copy[i] = src[i].clone();
+        }
+        return copy;
+    }
+
+    static Fish[] copyFish(Fish[] src) {
+        Fish[] copy = new Fish[17];
+        for (int i = 1; i < src.length; i++) {
+            if (src[i] != null) {
+                Fish f = src[i];
+                copy[i] = new Fish(f.y, f.x, f.dir, f.alive);
+            }
+        }
+        return copy;
+    }
+
+    static void moveFish(int y, int x) {
+        for (int i = 1; i <= 16; i++) {
+            Fish f = fish[i];
+
+            if (f.alive == 0) continue;
+
+            int ny, nx;
+
+            while (true) {
+                ny = dy[f.dir] + f.y;
+                nx = dx[f.dir] + f.x;
+
+                if (!inRange(ny, nx) || (ny == y && nx == x)) f.dir = (f.dir + 1) % 8;
+                else break;
+            }
+
+            fish[arr[ny][nx]].y = f.y;
+            fish[arr[ny][nx]].x = f.x;
+
+            int tmp = arr[f.y][f.x];
+            arr[f.y][f.x] = arr[ny][nx];
+            arr[ny][nx] = tmp;
+
+            f.y = ny;
+            f.x = nx;
+        }
+    }
+
+    static boolean inRange(int y, int x) {
+        return (y >= 0 && y < 4) && (x >= 0 && x < 4);
+    }
+}
+
+class Fish {
+    int y, x, dir, alive;
+
+    Fish(int y, int x, int dir, int alive) {
+        this.y = y;
+        this.x = x;
+        this.dir = dir;
+        this.alive = alive;
+    }
 }
