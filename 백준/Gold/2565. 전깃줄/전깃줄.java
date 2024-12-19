2915cbe85a305ed9fc9229f89722b2033c8ba850
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-12-19 13:26:42 +0900

    [Gold V] Title: 전깃줄, Time: 196 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, arr[][], dp[], ans;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        solve();
+
+        System.out.println(N - ans);
+    }
+
+    static void solve() {
+        Arrays.sort(arr, Comparator.comparing(a -> a[0]));
+
+        dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+    }
+}
