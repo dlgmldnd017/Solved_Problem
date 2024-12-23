cf808f769d34a4c41cedde3bd5dedb1fbbf5fcc4
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-12-23 16:09:38 +0900

    [Gold III] Title: 게리맨더링, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,113 +1,107 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N, ans;
-	static int p[], map[][];
-	
-	static boolean visited[];
+    static int N, pop[], ans;
+    static List<Integer> link[], A, B;
 
-	static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
 
-		N = Integer.parseInt(sc.readLine());
-		p = new int[N];
-		
-		st = new StringTokenizer(sc.readLine());
-		for(int i=0; i<N; i++) {
-			p[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		map = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(sc.readLine());
-			int size = Integer.parseInt(st.nextToken());
-			
-			for(int j=0; j<size; j++) {
-				int end = Integer.parseInt(st.nextToken())-1;
-				map[i][end]=1;
-				map[end][i]=1;
-			}
-		}
-		
-		ans = Integer.MAX_VALUE;
-		visited = new boolean[N];
-		
-		solve(0);
-		if(ans==Integer.MAX_VALUE) ans=-1;
-		System.out.println(ans);
-	}
+        pop = new int[N + 1];
 
-	static void solve(int depth) {
-		if(depth==N) {
-			List<Integer> listA = new ArrayList<>();
-			List<Integer> listB = new ArrayList<>();
-			
-			for(int i=0; i<N; i++) {
-				if(visited[i]) listA.add(i);
-				else listB.add(i);
-			}
-			
-			if(listA.size()==0 || listB.size()==0) return;
-			
-			if(!check(listA) || !check(listB)) return;
-			
-			int sumA=0, sumB=0;
-			
-			for(int i : listA) {
-				sumA += p[i];
-			}
-			
-			for(int i : listB) {
-				sumB += p[i];
-			}
-			
-			int diff = Math.abs(sumA-sumB);
-			ans = Math.min(ans, diff);
-			
-			return;
-		}
-		
-		visited[depth]=true;
-		solve(depth+1);
-		
-		visited[depth]=false;
-		solve(depth+1);
-	}
-	
-	static boolean check(List<Integer> list) {
-		if(list.size()==1) return true;
-		
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		q.add(list.get(0));
-		
-		boolean visited2[] = new boolean[N];
-		visited2[list.get(0)]=true;
-		
-		int cnt=1;
-		while(!q.isEmpty()) {
-			int start = q.poll();
-			
-			if(cnt==list.size()) return true;
-			
-			for(int next : list) {
-				if(!visited2[next] && map[start][next]==1) {
-					cnt++;
-					q.add(next);
-					visited2[next]=true;
-				}
-			}
-		}
-		
-		return false;
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) pop[i] = Integer.parseInt(st.nextToken());
+
+        link = new List[N + 1];
+
+        for (int i = 1; i <= N; i++) link[i] = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < M; j++) {
+                int V = Integer.parseInt(st.nextToken());
+
+                link[i].add(V);
+                link[V].add(i);
+            }
+        }
+
+        A = new ArrayList<>();
+        B = new ArrayList<>();
+
+        ans = Integer.MAX_VALUE;
+
+        solve(1);
+
+        if (ans == Integer.MAX_VALUE) ans = -1;
+        System.out.println(ans);
+    }
+
+    static void solve(int depth) {
+        if (depth == N + 1) {
+            if (A.isEmpty() || B.isEmpty()) return;
+
+            if (!checkConnected(A) || !checkConnected(B)) return;
+
+            int a = getSum(A);
+            int b = getSum(B);
+            int diff = Math.abs(a - b);
+
+            if (ans > diff) ans = diff;
+
+            return;
+        }
+
+        A.add(depth);
+        solve(depth + 1);
+        A.remove(A.size() - 1);
+
+        B.add(depth);
+        solve(depth + 1);
+        B.remove(B.size() - 1);
+    }
+
+    static boolean checkConnected(List<Integer> list) {
+        if (list.size() == 1) return true;
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(list.get(0));
+
+        boolean visited[] = new boolean[N + 1];
+
+        int cnt = 0;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (visited[cur]) continue;
+            visited[cur] = true;
+            cnt++;
+
+            if (cnt == list.size()) return true;
+
+            for (int next : link[cur]) {
+                if (visited[next] || !list.contains(next)) continue;
+
+                q.add(next);
+            }
+        }
+
+        return false;
+    }
+
+    static int getSum(List<Integer> list) {
+        int sum = 0;
+
+        for (int i : list) sum += pop[i];
+
+        return sum;
+    }
 }
