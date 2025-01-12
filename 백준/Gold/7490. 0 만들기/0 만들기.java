d8fb20685ba2ffb361b8f6b00eaafb116fab7123
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-01-13 02:07:39 +0900

    [Gold V] Title: 0 만들기, Time: 76 ms, Memory: 12660 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
@@ -6,11 +5,11 @@ import java.util.*;
 public class Main {
     static int T, N;
     static char op[];
-
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         T = Integer.parseInt(br.readLine());
 
@@ -29,10 +28,10 @@ public class Main {
 
     static void solve(int depth) {
         if (depth == N - 1) {
-            if (!check()) return;
-
-            for (int i = 1; i < N; i++) sb.append(i + "" + op[i - 1]);
-            sb.append(N + "\n");
+            if (isZero()) {
+                for (int i = 1; i < N; i++) sb.append(i + "" + op[i - 1]);
+                sb.append(N + "\n");
+            }
             return;
         }
 
@@ -46,9 +45,8 @@ public class Main {
         solve(depth + 1);
     }
 
-    static boolean check() {
+    static boolean isZero() {
         List<Integer> list = new ArrayList<>();
-
         int i = 0;
 
         while (i <= N - 1) {
@@ -63,20 +61,14 @@ public class Main {
             i++;
         }
 
-        int sum = list.get(0), idx = 1;
+        if (list.size() == 1) return false;
+
+        int sum = list.get(0), j = 1;
 
         for (i = 0; i < N - 1; i++) {
-            switch (op[i]) {
-                case '+':
-                    sum += list.get(idx);
-                    idx++;
-                    break;
-
-                case '-':
-                    sum -= list.get(idx);
-                    idx++;
-                    break;
-            }
+            if (op[i] == ' ') continue;
+            else if (op[i] == '+') sum += list.get(j++);
+            else sum -= list.get(j++);
         }
 
         return sum == 0;
