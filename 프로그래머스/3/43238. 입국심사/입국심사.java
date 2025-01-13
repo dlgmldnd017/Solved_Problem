daaca55024849ccd826448a00fabf124fbbce723
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-01-13 12:19:22 +0900

    [level 3] Title: 입국심사, Time: 53.11 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,19 +2,21 @@ class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         
-        long low = 1, high = (long) n * times[times.length-1];
+        long left = 0, right = (long) n * times[times.length - 1];
         
-        while(low <= high) {
-            long mid = (low + high) / 2, cnt = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2, cnt = 0;
             
-            for(int i = 0; i < times.length; i++) cnt += mid / times[i];
+            for (int i = 0; i < times.length; i++) cnt += mid / times[i];
             
-            if(cnt < n) low = mid + 1;
-            else {
-                high = mid - 1;
+            if (cnt >= n) {
+                right = mid - 1;
                 answer = mid;
+            } else {
+                left = mid + 1;
             }
         }
+        
         return answer;
     }
 }
