41eb246eec1faf3a29e2de0fd9cfa4bfd4658b49
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-01-16 16:18:15 +0900

    [Silver I] Title: 포도주 시식, Time: 92 ms, Memory: 13044 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    static int N, arr[], dp[], ans;
+    static int N, arr[], ans;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,28 +11,24 @@ public class Main {
 
         arr = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i < N + 1; i++) arr[i] = Integer.parseInt(br.readLine());
 
-        if (N == 1) {
-            System.out.println(arr[1]);
-            return;
-        }
-
-        dp = new int[N + 1];
-
-        dp[1] = arr[1];
-        dp[2] = arr[1] + arr[2];
-
-        solve();
+        if (N == 1) ans = arr[1];
+        else solve();
 
         System.out.println(ans);
     }
 
     static void solve() {
+        int dp[] = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+
         for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i] + arr[i - 1]));
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], arr[i - 1] + arr[i] + dp[i - 3]));
         }
 
         ans = dp[N];
     }
-} 
+}
