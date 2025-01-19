137388902e03ff779a0962b1a6ae36e3e7f4d0f9
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-01-19 15:27:42 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 520 ms, Memory: 75364 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -2,9 +2,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
-    static int N, M, h[], hs[];
+    static int N, M, H[], prefixH[];
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
@@ -15,12 +14,12 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        h = new int[N + 1];
+        H = new int[N + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) h[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) H[i] = Integer.parseInt(st.nextToken());
 
-        hs = new int[N + 2];
+        prefixH = new int[N + 2];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -28,20 +27,20 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
-            hs[a] = hs[a] + k;
-            hs[b + 1] = hs[b + 1] - k;
+            prefixH[a] = prefixH[a] + k;
+            prefixH[b + 1] = prefixH[b + 1] - k;
         }
 
         solve();
-
-        for (int i = 1; i <= N; i++) sb.append(h[i] + " ");
 
         System.out.println(sb);
     }
 
     static void solve() {
-        for (int i = 1; i <= N; i++) hs[i] = hs[i - 1] + hs[i];
+        for (int i = 1; i <= N; i++) prefixH[i] = prefixH[i - 1] + prefixH[i];
 
-        for (int i = 1; i <= N; i++) h[i] += hs[i];
+        for (int i = 1; i <= N; i++) H[i] += prefixH[i];
+
+        for (int i = 1; i <= N; i++) sb.append(H[i] + " ");
     }
 }
