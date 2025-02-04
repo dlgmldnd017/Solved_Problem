3fd260605295922da6fc2e4ac3f53ef351354857
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-02-04 17:54:28 +0900

    [Gold III] Title: 아기 상어, Time: 148 ms, Memory: 21856 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -3,9 +3,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, arr[][], cnt, ans;
+    static int N, arr[][], ans;
     static Shark shark;
-
     static int dy[] = {0, 0, -1, 1};
     static int dx[] = {-1, 1, 0, 0};
 
@@ -17,18 +16,19 @@ public class Main {
 
         arr = new int[N][N];
 
+        int fishCnt = 0;
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
 
-                if (arr[i][j] == 0) continue;
-                else if (arr[i][j] == 9) shark = new Shark(i, j, 2, 0);
-                else cnt++;
+                if (arr[i][j] == 9) shark = new Shark(i, j, 2, 0);
+                else if (1 <= arr[i][j] && arr[i][j] <= 6) fishCnt++;
             }
         }
 
-        if (cnt != 0) solve();
+        if (fishCnt != 0) solve();
 
         System.out.println(ans);
     }
@@ -38,55 +38,64 @@ public class Main {
 
         while (true) {
             Queue<Shark> q = new ArrayDeque<>();
-            q.add(new Shark(shark.y, shark.x, 0));
+            q.add(new Shark(shark.y, shark.x, 0, 0));
 
             boolean visited[][] = new boolean[N][N];
             visited[shark.y][shark.x] = true;
 
-            List<Fish> fish = new ArrayList<>();
+            PriorityQueue<Fish> pq = new PriorityQueue<>();
 
             while (!q.isEmpty()) {
                 Shark cur = q.poll();
 
                 for (int k = 0; k < 4; k++) {
-                    int ny = dy[k] + cur.y;
-                    int nx = dx[k] + cur.x;
+                    int ny = cur.y + dy[k];
+                    int nx = cur.x + dx[k];
 
                     if (!inRange(ny, nx) || visited[ny][nx] || shark.size < arr[ny][nx]) continue;
-
-                    if (arr[ny][nx] != 0 && shark.size > arr[ny][nx]) fish.add(new Fish(ny, nx, cur.cnt + 1));
                     visited[ny][nx] = true;
-                    q.add(new Shark(ny, nx, cur.cnt + 1));
+
+                    if (arr[ny][nx] != 0 && shark.size > arr[ny][nx]) pq.add(new Fish(ny, nx, cur.eatCnt + 1));
+                    q.add(new Shark(ny, nx, 0, cur.eatCnt + 1));
                 }
             }
 
-            if (fish.isEmpty()) return;
+            if (pq.isEmpty()) return;
 
-            Collections.sort(fish);
+            Fish fish = pq.poll();
+            shark.y = fish.y;
+            shark.x = fish.x;
+            shark.eatCnt++;
 
-            Fish f = fish.get(0);
-            shark.y = f.y;
-            shark.x = f.x;
-            shark.cnt++;
-
-            if (shark.size == shark.cnt) {
+            if (shark.size == shark.eatCnt) {
                 shark.size++;
-                shark.cnt = 0;
+                shark.eatCnt = 0;
             }
 
-            arr[f.y][f.x] = 0;
+            arr[shark.y][shark.x] = 0;
 
-            ans += f.dist;
+            ans += fish.dist;
         }
     }
 
     static boolean inRange(int y, int x) {
-        return (y >= 0 && y < N) && (x >= 0 && x < N);
+        return (0 <= y && y < N) && (0 <= x && x < N);
+    }
+}
+
+class Shark {
+    int y, x, size, eatCnt;
+
+    Shark(int y, int x, int size, int eatCnt) {
+        this.y = y;
+        this.x = x;
+        this.size = size;
+        this.eatCnt = eatCnt;
     }
 }
 
 class Fish implements Comparable<Fish> {
-    int y, x, dist;
+    int y, x, size, dist;
 
     Fish(int y, int x, int dist) {
         this.y = y;
@@ -94,28 +103,11 @@ class Fish implements Comparable<Fish> {
         this.dist = dist;
     }
 
-    public int compareTo(Fish f) {
-        if (this.dist == f.dist) {
-            if (this.y == f.y) return this.x - f.x;
-            return this.y - f.y;
+    public int compareTo(Fish n) {
+        if (this.dist == n.dist) {
+            if (this.y == n.y) return this.x - n.x;
+            return this.y - n.y;
         }
-        return this.dist - f.dist;
-    }
-}
-
-class Shark {
-    int y, x, size, cnt;
-
-    Shark(int y, int x, int size, int cnt) {
-        this.y = y;
-        this.x = x;
-        this.size = size;
-        this.cnt = cnt;
-    }
-
-    Shark(int y, int x, int cnt) {
-        this.y = y;
-        this.x = x;
-        this.cnt = cnt;
+        return this.dist - n.dist;
     }
 }
