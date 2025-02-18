ce23d628c8f5c9419c8290658196aa01a2744baf
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-02-18 11:32:12 +0900

    [Gold IV] Title: 소트, Time: 64 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.java
+++ 백준/Gold/1083. 소트/소트.java
@@ -3,8 +3,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, A[], S;
-
+    static int N, S;
+    static int[] A;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
