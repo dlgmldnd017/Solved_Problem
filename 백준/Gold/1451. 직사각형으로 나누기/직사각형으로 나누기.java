f54a90670fa0ef9da5907ad274af19b3b94baf45
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-02-18 23:26:21 +0900

    [Gold IV] Title: 직사각형으로 나누기, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Gold/1451. 직사각형으로 나누기/직사각형으로 나누기.java
+++ 백준/Gold/1451. 직사각형으로 나누기/직사각형으로 나누기.java
@@ -3,7 +3,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M, arr[][], prefixSum[][];
+    static int N, M;
+    static int[][] arr, prefixSum;
     static long ans;
 
     public static void main(String[] args) throws Exception {
@@ -16,10 +17,12 @@ public class Main {
 
         arr = new int[N + 1][M + 1];
 
-        for (int i = 1; i <= N; i++) {
-            String str = br.readLine();
-            for (int j = 1; j <= M; j++) arr[i][j] = str.charAt(j - 1) - '0';
+        for (int i = 1; i < N + 1; i++) {
+            String input = br.readLine();
+            for (int j = 1; j < M + 1; j++) arr[i][j] = input.charAt(j - 1) - '0';
         }
+
+        prefixSum = new int[N + 1][M + 1];
 
         solve();
 
@@ -27,9 +30,7 @@ public class Main {
     }
 
     static void solve() {
-        // 0. 누적합 구하기
-        prefixSum = new int[N + 1][M + 1];
-        calcPreSum();
+        calcPrefixSum();
 
         // 1. 가로로 3개 나누기
         for (int i = 1; i < N; i++) {
@@ -51,14 +52,16 @@ public class Main {
             }
         }
 
-        // 3. 가로 1개, 세로 2개 나누기
+        // 3. 가로 고정
         for (int i = 1; i < N; i++) {
             for (int j = 1; j < M; j++) {
+                // 위쪽 가로 고정 
                 long a = getSum(1, 1, i, M);
                 long b = getSum(i + 1, 1, N, j);
                 long c = getSum(i + 1, j + 1, N, M);
                 ans = Math.max(ans, a * b * c);
 
+                // 아래쪽 가로 고정
                 a = getSum(1, 1, i, j);
                 b = getSum(1, j + 1, i, M);
                 c = getSum(i + 1, 1, N, M);
@@ -66,14 +69,17 @@ public class Main {
             }
         }
 
-        // 4. 세로 1개, 가로 2개 나누기
+        // 4. 세로 고정
         for (int i = 1; i < M; i++) {
             for (int j = 1; j < N; j++) {
+
+                // 왼쪽 세로 고정
                 long a = getSum(1, 1, N, i);
                 long b = getSum(1, i + 1, j, M);
                 long c = getSum(j + 1, i + 1, N, M);
                 ans = Math.max(ans, a * b * c);
 
+                // 오른쪽 세로 고정
                 a = getSum(1, 1, j, i);
                 b = getSum(j + 1, 1, N, i);
                 c = getSum(1, i + 1, N, M);
@@ -82,21 +88,15 @@ public class Main {
         }
     }
 
-    static void calcPreSum() {
+    static void calcPrefixSum() {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
-                prefixSum[i][j] = arr[i][j]
-                        + prefixSum[i - 1][j]
-                        + prefixSum[i][j - 1]
-                        - prefixSum[i - 1][j - 1];
+                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
             }
         }
     }
 
     static long getSum(int x1, int y1, int x2, int y2) {
-        return prefixSum[x2][y2]
-                - prefixSum[x1 - 1][y2]
-                - prefixSum[x2][y1 - 1]
-                + prefixSum[x1 - 1][y1 - 1];
+        return prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
     }
 }
