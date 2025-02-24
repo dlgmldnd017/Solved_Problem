5416f4d51194374fd65d32629e973c57af5b0991
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-02-24 14:05:33 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1116 ms, Memory: 16316 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static List<String> words = new ArrayList<>();
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) words.add(br.readLine());
+
+        solve();
+
+        System.out.println(sb);
+    }
+
+    static void solve() {
+        int idx1 = -1, idx2 = -1, max = -1;
+
+        for (int i = 0; i < N - 1; i++) {
+            String s1 = words.get(i);
+
+            for (int j = i + 1; j < N; j++) {
+                String s2 = words.get(j);
+                int cnt = check(s1, s2);
+
+                if (cnt > max) {
+                    idx1 = i;
+                    idx2 = j;
+                    max = cnt;
+                }
+            }
+        }
+
+        sb.append(words.get(idx1)).append("\n").append(words.get(idx2));
+    }
+
+    static int check(String s1, String s2) {
+        int cnt = 0, size = Math.min(s1.length(), s2.length());
+        
+        for (int i = 0; i < size; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) break;
+            cnt++;
+        }
+        
+        return cnt;
+    }
+}
