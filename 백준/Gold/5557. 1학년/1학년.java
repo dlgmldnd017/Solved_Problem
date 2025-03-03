f82610cb1953dafb02a6875fe8a102a5ddae3373
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-03-03 17:11:50 +0900

    [Gold V] Title: 1학년, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -3,15 +3,15 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, arr[];
+    static int N;
+    static int[] arr;
     static long ans;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
 
         arr = new int[N];
 
@@ -24,10 +24,17 @@ public class Main {
     }
 
     static void solve() {
-        long dp[][] = new long[N][21];
+        // (2^63) - 1이므로, long 배열 타입 선언
+        long[][] dp = new long[N][21];
+
+        // 첫 번째 경우의 수 삽입
         dp[0][arr[0]] = 1;
 
+        // 모든 경우의 수 구하기
         for (int i = 1; i < N - 1; i++) {
+
+            // i는 현재 원소 값
+            // j는 이전 결과 값 또는 이전 원소 값
             for (int j = 0; j <= 20; j++) {
                 if (dp[i - 1][j] <= 0) continue;
 
@@ -39,6 +46,7 @@ public class Main {
             }
         }
 
-        ans = dp[N-2][arr[N-1]];
+        // 마지막 두 숫자 사이에 =을 넣어야 하므로, 정답은 아래와 같다.
+        ans = dp[N - 2][arr[N - 1]];
     }
 }
