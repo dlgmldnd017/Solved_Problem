8b68210732cb44bf5722274e3945f217169ba1f1
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-03-05 10:26:12 +0900

    [Gold III] Title: 경사로, Time: 84 ms, Memory: 12900 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, L, arr[][], ans;
+    static int N, L, ans;
+    static int[][] arr;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -20,43 +22,54 @@ public class Main {
             for (int j = 0; j < N; j++) arr[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < N; i++) {
-            if (solve(arr[i])) ans++;
-            if (solve(getColumn(i))) ans++;
-        }
+        solve();
 
         System.out.println(ans);
     }
 
-    static boolean solve(int line[]) {
-        boolean visited[] = new boolean[N];
+    static void solve() {
+        for (int i = 0; i < N; i++) {
+            if (canGo(arr[i])) ans++;
+            if (canGo(getCol(i))) ans++;
+        }
+    }
+
+    static boolean canGo(int[] way) {
+        boolean[] visited = new boolean[N];
 
         for (int i = 0; i < N - 1; i++) {
-            int diff = line[i] - line[i + 1];
-
-            if (diff == 0) continue;
+            int diff = way[i] - way[i + 1];
 
             if (Math.abs(diff) > 1) return false;
 
-            if (diff == 1) {
-                for (int j = i + 1; j <= i + L; j++) {
-                    if (j == N || line[j] != line[i + 1] || visited[j]) return false;
-                    visited[j] = true;
+            if (diff == 0) continue;
+
+            if (diff > 0) {
+                if (visited[i + 1]) return false;
+                visited[i + 1] = true;
+
+                for (int j = i + 1; j < i + L; j++) {
+                    if (j + 1 >= N || way[j] != way[j + 1] || visited[j + 1]) return false;
+                    visited[j + 1] = true;
                 }
             } else {
-                for (int j = i; j > i - L; j--) {
-                    if (j < 0 || line[j] != line[i] || visited[j]) return false;
-                    visited[j] = true;
+                if (visited[i]) return false;
+                visited[i] = true;
+
+                for (int j = i; j > i - L + 1; j--) {
+                    if (j - 1 < 0 || way[j] != way[j - 1] || visited[j - 1]) return false;
+                    visited[j - 1] = true;
                 }
             }
         }
+
         return true;
     }
 
-    static int[] getColumn(int i) {
-        int tmp[] = new int[N];
+    static int[] getCol(int j) {
+        int[] tmp = new int[N];
 
-        for (int j = 0; j < N; j++) tmp[j] = arr[j][i];
+        for (int i = 0; i < N; i++) tmp[i] = arr[i][j];
 
         return tmp;
     }
