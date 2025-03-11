619bafc4e6df535c32e99be78462871b3ce159b9
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-03-11 12:08:41 +0900

    [Gold IV] Title: 빙산, Time: 488 ms, Memory: 77824 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -3,12 +3,14 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M, arr[][], ans;
+    static int N, M, ans;
+    static int[][] arr;
+    static List<Node> list = new ArrayList<>();
 
-    static int dy[] = {0, 0, -1, 1};
-    static int dx[] = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1, 1, 0, 0};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -20,7 +22,11 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+                if (arr[i][j] != 0) list.add(new Node(i, j, arr[i][j]));
+            }
         }
 
         solve();
@@ -29,79 +35,96 @@ public class Main {
     }
 
     static void solve() {
-        int i = 0;
+        int t = 0;
 
-        while (true) {
-            int cnt = checkArea();
-
-            if (cnt == 0) return;
-            else if (cnt >= 2) {
-                ans = i;
+        while (!list.isEmpty()) {
+            if (check()) {
+                ans = t;
                 return;
             }
 
-            i++;
+            melt();
+
+            t++;
         }
     }
 
-    static int checkArea() {
-        int cnt = 0;
-
-        boolean visited[][] = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0 || visited[i][j]) continue;
-
-                bfs(i, j, visited);
-                cnt++;
-            }
-        }
-
-        return cnt;
-    }
-
-    static void bfs(int y, int x, boolean visited[][]) {
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(y, x));
-
-        visited[y][x] = true;
-
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-
+    static void melt() {
+        for (Node n : list) {
             int cnt = 0;
 
             for (int k = 0; k < 4; k++) {
-                int ny = dy[k] + cur.y;
-                int nx = dx[k] + cur.x;
+                int ny = n.y + dy[k];
+                int nx = n.x + dx[k];
 
-                if (!inRange(ny, nx) || visited[ny][nx]) continue;
+                if (!inRange(ny, nx) || arr[ny][nx] != 0) continue;
 
-                if (arr[ny][nx] == 0) {
-                    cnt++;
-                    continue;
-                }
-
-                visited[ny][nx] = true;
-                q.add(new Node(ny, nx));
+                cnt++;
             }
 
-            if (arr[cur.y][cur.x] - cnt < 0) arr[cur.y][cur.x] = 0;
-            else arr[cur.y][cur.x] -= cnt;
+            n.num = Math.max(n.num - cnt, 0);
+        }
+
+        int i = 0;
+
+        while (i < list.size()) {
+            Node n = list.get(i);
+
+            arr[n.y][n.x] = n.num;
+
+            if (n.num == 0) list.remove(i);
+            else i++;
         }
     }
 
+    static boolean check() {
+        int cnt = 0;
+
+        boolean[][] visited = new boolean[N][M];
+
+        for (Node n : list) {
+            if (visited[n.y][n.x]) continue;
+            visited[n.y][n.x] = true;
+
+            cnt++;
+
+            Queue<Node> q = new ArrayDeque<>();
+            q.add(new Node(n.y, n.x));
+
+            while (!q.isEmpty()) {
+                Node cur = q.poll();
+
+                for (int k = 0; k < 4; k++) {
+                    int ny = cur.y + dy[k];
+                    int nx = cur.x + dx[k];
+
+                    if (!inRange(ny, nx) || visited[ny][nx] || arr[ny][nx] == 0) continue;
+
+                    visited[ny][nx] = true;
+                    q.add(new Node(ny, nx));
+                }
+            }
+        }
+
+        return cnt >= 2;
+    }
+
     static boolean inRange(int y, int x) {
-        return (y >= 0 && y < N) && (x >= 0 && x < M);
+        return (0 <= y && y < N) && (0 <= x && x < M);
     }
 }
 
 class Node {
-    int y, x;
+    int y, x, num;
 
     Node(int y, int x) {
         this.y = y;
         this.x = x;
+    }
+
+    Node(int y, int x, int num) {
+        this.y = y;
+        this.x = x;
+        this.num = num;
     }
 }
