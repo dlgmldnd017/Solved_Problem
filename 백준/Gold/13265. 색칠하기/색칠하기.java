7f38bf302d5631065d580c74791167e5e3b9cd09
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-03-28 17:14:09 +0900

    [Gold V] Title: 색칠하기, Time: 604 ms, Memory: 143824 KB -BaekjoonHub

--- 백준/Gold/13265. 색칠하기/색칠하기.java
+++ 백준/Gold/13265. 색칠하기/색칠하기.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] color;
-    static boolean[] visited;
-    static List<Node> list[];
+    static List<Integer>[] list;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String args[]) throws Exception {
@@ -21,68 +20,51 @@ public class Main {
             M = Integer.parseInt(st.nextToken());
 
             list = new ArrayList[N + 1];
-
             for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
 
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-
-                list[a].add(new Node(b, -1));
-                list[b].add(new Node(a, -1));
+                list[a].add(b);
+                list[b].add(a);
             }
 
-            color = new int[N + 1];
+            color = new int[N + 1]; // 0: 방문 안 함, 1: 색1, 2: 색2
 
-            visited = new boolean[N + 1];
-
-            solve();
+            if (solve()) sb.append("possible\n");
+            else sb.append("impossible\n");
         }
 
         System.out.println(sb);
     }
 
-    static void solve() {
+    static boolean solve() {
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) bfs(i);
-
-            for (Node n : list[i]) {
-                if (color[i] != color[n.e]) continue;
-                sb.append("impossible").append("\n");
-                return;
+            if (color[i] == 0) {
+                if (!bfs(i)) return false;
             }
         }
-
-        sb.append("possible").append("\n");
+        return true;
     }
 
-    static void bfs(int start) {
-        visited[start] = true;
-
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(start, 1));
+    static boolean bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(start);
+        color[start] = 1; // 첫 번째 색 지정
 
         while (!q.isEmpty()) {
-            Node cur = q.poll();
+            int cur = q.poll();
 
-            color[cur.e] = cur.c;
-
-            for (Node next : list[cur.e]) {
-                if (visited[next.e]) continue;
-                visited[next.e] = true;
-
-                q.add(new Node(next.e, cur.c == 1 ? 2 : 1));
+            for (int next : list[cur]) {
+                if (color[next] == 0) { // 방문하지 않은 경우
+                    color[next] = 3 - color[cur]; // 1 -> 2, 2 -> 1
+                    q.add(next);
+                } else if (color[next] == color[cur]) { // 같은 색상이면 이분 그래프가 아님
+                    return false;
+                }
             }
         }
-    }
-}
-
-class Node {
-    int e, c;
-
-    Node(int e, int c) {
-        this.e = e;
-        this.c = c;
+        return true;
     }
 }
