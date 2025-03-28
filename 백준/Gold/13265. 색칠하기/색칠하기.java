4b8dfbeb7bfd169132c213b262a2f82137a36690
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-03-28 17:20:30 +0900

    [Gold V] Title: 색칠하기, Time: 588 ms, Memory: 143628 KB -BaekjoonHub

--- 백준/Gold/13265. 색칠하기/색칠하기.java
+++ 백준/Gold/13265. 색칠하기/색칠하기.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] color;
-    static List<Integer>[] list;
+    static List<Integer> list[];
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String args[]) throws Exception {
@@ -20,51 +20,56 @@ public class Main {
             M = Integer.parseInt(st.nextToken());
 
             list = new ArrayList[N + 1];
+
             for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
 
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
+
                 list[a].add(b);
                 list[b].add(a);
             }
 
-            color = new int[N + 1]; // 0: 방문 안 함, 1: 색1, 2: 색2
+            color = new int[N + 1];
 
-            if (solve()) sb.append("possible\n");
-            else sb.append("impossible\n");
+            solve();
         }
 
         System.out.println(sb);
     }
 
-    static boolean solve() {
+    static void solve() {
         for (int i = 1; i <= N; i++) {
             if (color[i] == 0) {
-                if (!bfs(i)) return false;
+                if (!bfs(i)) {
+                    sb.append("impossible").append("\n");
+                    return;
+                }
             }
         }
-        return true;
+
+        sb.append("possible").append("\n");
     }
 
     static boolean bfs(int start) {
         Queue<Integer> q = new ArrayDeque<>();
         q.add(start);
-        color[start] = 1; // 첫 번째 색 지정
+
+        color[start] = 1;
 
         while (!q.isEmpty()) {
             int cur = q.poll();
 
             for (int next : list[cur]) {
-                if (color[next] == 0) { // 방문하지 않은 경우
-                    color[next] = 3 - color[cur]; // 1 -> 2, 2 -> 1
+                if (color[next] == 0) {
+                    color[next] = 3 - color[cur];
                     q.add(next);
-                } else if (color[next] == color[cur]) { // 같은 색상이면 이분 그래프가 아님
-                    return false;
-                }
+                } else if (color[cur] == color[next]) return false;
             }
         }
+
         return true;
     }
 }
