0d597693fda648e37f79b08fb4b79f85d8c4a6a5
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-04-02 18:22:48 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 184 ms, Memory: 13476 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,58 +1,67 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, arr[][], ans;
+    static int N, ans;
+    static int[][] eggs;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N][2];
+        eggs = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-
-            arr[i][0] = S;
-            arr[i][1] = W;
+            for (int j = 0; j < 2; j++) eggs[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        solve(0, 0);
+        solve();
 
         System.out.println(ans);
     }
 
-    static void solve(int idx, int cnt) {
-        if (idx == N) {
+    static void solve() {
+        if (N == 1) return;
+
+        // 1. 가장 왼쪽의 계란을 든다.
+        dfs(0, 0);
+    }
+
+    // depth : 가장 왼쪽의 계란
+    // cnt : 깨진 계란 개수
+    static void dfs(int depth, int cnt) {
+        // 3. 가장 최근에 든 계란이 오른쪽에 위치한 계란일 경우
+        if (depth == N) {
             if (ans < cnt) ans = cnt;
             return;
         }
 
-        if (arr[idx][0] <= 0 || cnt == N - 1) {
-            solve(idx + 1, cnt);
+        // 2. 손에 든 계란이 깨졌거나 깨지지 않은 계란이 없으면, 3번 진행
+        if (eggs[depth][0] <= 0 || cnt == N - 1) {
+            dfs(depth + 1, cnt);
             return;
         }
 
         int prevCnt = cnt;
 
+        // 2. 깨지지 않은 계란 중 하나 고름
         for (int i = 0; i < N; i++) {
-            if (idx == i || arr[i][0] <= 0) continue;
+            if (depth == i || eggs[i][0] <= 0) continue;
 
-            arr[idx][0] -= arr[i][1];
-            arr[i][0] -= arr[idx][1];
+            eggs[depth][0] -= eggs[i][1];
+            eggs[i][0] -= eggs[depth][1];
 
-            if (arr[idx][0] <= 0) cnt++;
-            if (arr[i][0] <= 0) cnt++;
+            if (eggs[depth][0] <= 0) cnt++;
+            if (eggs[i][0] <= 0) cnt++;
 
-            solve(idx + 1, cnt);
+            dfs(depth + 1, cnt);
 
-            arr[idx][0] += arr[i][1];
-            arr[i][0] += arr[idx][1];
+            eggs[depth][0] += eggs[i][1];
+            eggs[i][0] += eggs[depth][1];
 
             cnt = prevCnt;
         }
