535b89ebd91aba5640714e0e85b60d01d41016d1
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-04-14 18:24:21 +0900

    [Gold IV] Title: 무기 공학, Time: 148 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.java
+++ 백준/Gold/18430. 무기 공학/무기 공학.java
@@ -7,13 +7,6 @@ public class Main {
     static int[][] arr;
     static boolean[][] visited;
 
-    static int[][][] boo = {
-            {{0, 0}, {1, 0}, {0, -1}}, // ㄱ
-            {{0, 0}, {1, 0}, {0, 1}}, // ┌
-            {{0, 0}, {-1, 0}, {0, -1}}, // ┘
-            {{0, 0}, {-1, 0}, {0, 1}} // ㄴ
-    };
-
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -42,48 +35,56 @@ public class Main {
         dfs(0, 0, 0);
     }
 
-    static void dfs(int y, int x, int total) {
+    static void dfs(int y, int x, int sum) {
         if (y == N) {
-            if (ans < total) ans = total;
+            if (ans < sum) ans = sum;
             return;
         }
 
-        int ny = (x + 1 == M) ? y + 1 : y;
-        int nx = (x + 1 == M) ? 0 : x + 1;
-
-        if (!visited[y][x]) {
-            for (int[][] shape : boo) {
-                int[][] tmp = new int[3][2];
-
-                boolean canPlace = true;
-
-                int sum = 0;
-
-                for (int k = 0; k < 3; k++) {
-                    int dy = y + shape[k][0];
-                    int dx = x + shape[k][1];
-
-                    if (!inRange(dy, dx) || visited[dy][dx]) {
-                        canPlace = false;
-                        break;
-                    }
-
-                    tmp[k][0] = dy;
-                    tmp[k][1] = dx;
-
-                    if (k == 0) sum += arr[dy][dx] * 2;
-                    else sum += arr[dy][dx];
-                }
-
-                if (canPlace) {
-                    for (int[] t : tmp) visited[t[0]][t[1]] = true;
-                    dfs(ny, nx, total + sum);
-                    for (int[] t : tmp) visited[t[0]][t[1]] = false;
-                }
-            }
+        if (x == M) {
+            dfs(y + 1, 0, sum);
+            return;
         }
 
-        dfs(ny, nx, total);
+        if (!visited[y][x]) {
+            visited[y][x] = true;
+
+            if (inRange(y + 1, x - 1) && !visited[y + 1][x] && !visited[y][x - 1]) {
+                visited[y + 1][x] = true;
+                visited[y][x - 1] = true;
+                dfs(y, x + 1, sum + arr[y][x] * 2 + arr[y + 1][x] + arr[y][x - 1]);
+                visited[y + 1][x] = false;
+                visited[y][x - 1] = false;
+            }
+
+            if (inRange(y - 1, x - 1) && !visited[y - 1][x] && !visited[y][x - 1]) {
+                visited[y - 1][x] = true;
+                visited[y][x - 1] = true;
+                dfs(y, x + 1, sum + arr[y][x] * 2 + arr[y - 1][x] + arr[y][x - 1]);
+                visited[y - 1][x] = false;
+                visited[y][x - 1] = false;
+            }
+
+            if (inRange(y - 1, x + 1) && !visited[y - 1][x] && !visited[y][x + 1]) {
+                visited[y - 1][x] = true;
+                visited[y][x + 1] = true;
+                dfs(y, x + 1, sum + arr[y][x] * 2 + arr[y - 1][x] + arr[y][x + 1]);
+                visited[y - 1][x] = false;
+                visited[y][x + 1] = false;
+            }
+
+            if (inRange(y + 1, x + 1) && !visited[y + 1][x] && !visited[y][x + 1]) {
+                visited[y + 1][x] = true;
+                visited[y][x + 1] = true;
+                dfs(y, x + 1, sum + arr[y][x] * 2 + arr[y + 1][x] + arr[y][x + 1]);
+                visited[y + 1][x] = false;
+                visited[y][x + 1] = false;
+            }
+
+            visited[y][x] = false;
+        }
+
+        dfs(y, x + 1, sum);
     }
 
     static boolean inRange(int y, int x) {
