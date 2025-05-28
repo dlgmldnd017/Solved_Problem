08d5b58449d85f0aad49a767ea54d9738f1cece2
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-05-28 15:33:11 +0900

    [Gold V] Title: 순열의 순서, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,12 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int N;
-    static int[] per;
     static long[] fac;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
@@ -18,6 +15,7 @@ public class Main {
         N = Integer.parseInt(br.readLine());
 
         fac = new long[N + 1];
+
         fac[0] = 1;
 
         for (int i = 1; i <= N; i++) fac[i] = fac[i - 1] * i;
@@ -25,46 +23,40 @@ public class Main {
         visited = new boolean[N + 1];
 
         st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
 
-        int type = Integer.parseInt(st.nextToken());
-
-        if (type == 1) {
+        if (num == 1) {
             long k = Long.parseLong(st.nextToken());
 
-            List<Integer> list = new ArrayList<>();
-
             for (int i = 0; i < N; i++) {
-                long count = fac[N - 1 - i];
+                long cnt = fac[N - 1 - i];
 
                 for (int j = 1; j <= N; j++) {
                     if (visited[j]) continue;
-                    
-                    if (count < k) {
-                        k -= count;
-                    } else {
-                        list.add(j);
+
+                    if (cnt < k) k -= cnt;
+                    else {
+                        sb.append(j).append(" ");
                         visited[j] = true;
                         break;
                     }
                 }
             }
-
-            for (int i : list) sb.append(i).append(" ");
         } else {
-            per = new int[N];
-            visited = new boolean[N + 1];
+            int[] arr = new int[N];
             long k = 1;
 
             for (int i = 0; i < N; i++) {
-                per[i] = Integer.parseInt(st.nextToken());
+                arr[i] = Integer.parseInt(st.nextToken());
 
-                int count = 0;
-                for (int j = 1; j < per[i]; j++) {
-                    if (!visited[j]) count++;
+                int cnt = 0;
+
+                for (int j = 1; j < arr[i]; j++) {
+                    if (!visited[j]) cnt++;
                 }
 
-                k += count * fac[N - 1 - i];
-                visited[per[i]] = true;
+                visited[arr[i]] = true;
+                k += cnt * fac[N - 1 - i];
             }
 
             sb.append(k);
