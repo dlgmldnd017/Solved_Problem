7f46cfc77e655acacb08be27f60fc1e4c0da29f0
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-05-29 22:35:47 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1972 ms, Memory: 358184 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -2,25 +2,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Node implements Comparable<Node> {
-    int e, c;
-
-    Node(int e, int c) {
-        this.e = e;
-        this.c = c;
-    }
-
-    public int compareTo(Node n) {
-        return this.c - n.c;
-    }
-}
-
 public class Main {
     static int N, M, ans;
-    static List<Node> list[];
-    static List<Integer> list2 = new ArrayList<>();
+    static int[] arr;
+    static List<Node>[] list;
+    static List<Integer> cost = new ArrayList<>();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -28,25 +16,25 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        arr = new int[N + 1];
         list = new ArrayList[N + 1];
 
-        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.MAX_VALUE;
+            list[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            list[start].add(new Node(end, cost));
-            list[end].add(new Node(start, cost));
+            list[y].add(new Node(x, c));
+            list[x].add(new Node(y, c));
         }
 
         solve();
-
-        Collections.sort(list2);
-
-        for (int i = 1; i < list2.size() -1 ; i++) ans += list2.get(i);
 
         System.out.println(ans);
     }
@@ -57,19 +45,39 @@ public class Main {
 
         boolean visited[] = new boolean[N + 1];
 
+        arr[1] = 0;
+
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
 
             if (visited[cur.e]) continue;
             visited[cur.e] = true;
 
-            list2.add(cur.c);
+            cost.add(cur.c);
 
             for (Node n : list[cur.e]) {
-                if (visited[n.e]) continue;
+                if (visited[n.e] || arr[n.e] < n.c) continue;
 
+                arr[n.e] = n.c;
                 pq.add(new Node(n.e, n.c));
             }
         }
+
+        Collections.sort(cost);
+
+        for (int i = 1; i < cost.size() - 1; i++) ans += cost.get(i);
+    }
+}
+
+class Node implements Comparable<Node> {
+    int e, c;
+
+    Node(int e, int c) {
+        this.e = e;
+        this.c = c;
+    }
+
+    public int compareTo(Node n) {
+        return this.c - n.c;
     }
 }
