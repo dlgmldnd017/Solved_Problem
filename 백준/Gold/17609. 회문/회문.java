e17bffd85f8d94e17032c3737ec158ea432fdf28
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-06-08 13:07:19 +0900

    [Gold V] Title: 회문, Time: 220 ms, Memory: 44240 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -11,42 +11,40 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
-            String input = br.readLine();
-
-            ch = input.toCharArray();
+            ch = br.readLine().toCharArray();
 
             solve();
         }
-
+        
         System.out.println(sb);
     }
 
     static void solve() {
         int left = 0, right = ch.length - 1;
-
+        
         while (left < right) {
             if (ch[left] != ch[right]) {
                 if (check(left + 1, right) || check(left, right - 1)) sb.append("1\n");
                 else sb.append("2\n");
-
+                
                 return;
             }
-
+            
             left++;
             right--;
         }
-
+        
         sb.append("0\n");
     }
-
+    
     static boolean check(int left, int right) {
         while(left < right) {
             if (ch[left] != ch[right]) return false;
-
+            
             left++;
             right--;
         }
-
+        
         return true;
     }
 }
