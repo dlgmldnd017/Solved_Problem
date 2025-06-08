7a2d52eb9a2d6c7de07de8d184dc993fca105225
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-06-08 14:26:00 +0900

    [Gold V] Title: 회문, Time: 272 ms, Memory: 39692 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -2,49 +2,51 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static char[] ch;
+    static int N;
+    static String str;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        for (int t = 0; t < T; t++) {
-            ch = br.readLine().toCharArray();
+        for (int i = 0; i < N; i++) {
+            str = br.readLine();
 
             solve();
         }
-        
+
         System.out.println(sb);
     }
 
     static void solve() {
-        int left = 0, right = ch.length - 1;
-        
+        int left = 0, right = str.length() - 1;
+
         while (left < right) {
-            if (ch[left] != ch[right]) {
-                if (check(left + 1, right) || check(left, right - 1)) sb.append("1\n");
-                else sb.append("2\n");
-                
+            if (str.charAt(left) != str.charAt(right)) {
+                if (check(left + 1, right) || check(left, right - 1)) sb.append("1").append("\n");
+                else sb.append("2").append("\n");
                 return;
             }
-            
+
             left++;
             right--;
         }
-        
-        sb.append("0\n");
+
+        sb.append("0").append("\n");
     }
-    
+
     static boolean check(int left, int right) {
-        while(left < right) {
-            if (ch[left] != ch[right]) return false;
-            
+        if (left > right) return false;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) return false;
+
             left++;
             right--;
         }
-        
+
         return true;
     }
 }
