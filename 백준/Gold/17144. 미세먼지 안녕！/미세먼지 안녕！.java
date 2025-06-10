cde56562eb8efe6c130247df0633886e27017ccb
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-06-10 23:47:57 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 308 ms, Memory: 39316 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -3,198 +3,110 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int R, C, T;
-    static int r[], c[];
-    static int map[][], tmp[][];
+    static int R, C, T, air, ans;
+    static int[][] A;
 
-    static int dy[] = {-1, 0, 1, 0};
-    static int dx[] = {0, 1, 0, -1};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1, 1, 0, 0};
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(sc.readLine());
+        st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
 
-        r = new int[2];
-        c = new int[2];
-        map = new int[R][C];
+        A = new int[R][C];
 
-        int idx = 0;
         for (int i = 0; i < R; i++) {
-            st = new StringTokenizer(sc.readLine());
-
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < C; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());
 
-                if (map[i][j] == -1) {
-                    r[idx] = i;
-                    c[idx] = j;
-                    idx++;
-                }
+                if (A[i][j] == -1) air = i;
+                else ans += A[i][j];
             }
         }
+
+        air -= 1;
 
         solve();
-        System.out.println(countDust());
-    }
 
-    static int countDust() {
-        int sum = 0;
-
-        for (int i[] : map) {
-            for (int j : i) {
-                if (j == 0 || j == -1) continue;
-
-                sum += j;
-            }
-        }
-
-        return sum;
+        System.out.println(ans);
     }
 
     static void solve() {
-        for (int t = 1; t <= T; t++) {
-            tmp = new int[R][C];
+        for (int t = 0; t < T; t++) {
             spreadDust();
 
-            tmp = new int[R][C];
             excuteAirCleaner();
         }
+
     }
 
     static void spreadDust() {
+        int[][] tmp = new int[R][C];
+
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if (map[i][j] == 0) continue;
+                if (A[i][j] == -1 || A[i][j] == 0) continue;
 
-                int sum = 0;
-                
+                int value = A[i][j] / 5, cnt = 0;
+
                 for (int k = 0; k < 4; k++) {
                     int ny = i + dy[k];
                     int nx = j + dx[k];
 
-                    if (!inRange(ny, nx) || map[ny][nx] == -1) continue;
+                    if (!inRange(ny, nx) || A[ny][nx] == -1) continue;
 
-                    sum += map[i][j] / 5;
-
-                    tmp[ny][nx] += map[i][j] / 5;
+                    cnt++;
+                    tmp[ny][nx] += value;
                 }
 
-                map[i][j] -= sum;
+                tmp[i][j] += A[i][j] - (value * cnt);
             }
         }
 
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (tmp[i][j] == 0) continue;
-                map[i][j] += tmp[i][j];
-            }
-        }
+        for (int i = 0; i < R; i++) A[i] = tmp[i].clone();
+
+        A[air][0] = A[air + 1][0] = -1;
     }
-    
+
     static void excuteAirCleaner() {
-        int r1 = r[0], c1 = c[0];
-        int printX = C - 1, printY = r1, t = 1;
+        // 반시계
+        ans -= A[air - 1][0];
 
-        for (int k = 0; k < 2; k++) {
+        A[air - 1][0] = 0;
 
-            for (int i = 0; i < printX; i++) {
-                int dust = map[r1][c1];
-                c1 += t;
+        for (int i = air - 1; i > 0; i--) A[i][0] = A[i - 1][0];
 
-                if (dust == -1 || dust == 0) continue;
+        for (int i = 0; i < C - 1; i++) A[0][i] = A[0][i + 1];
 
-                tmp[r1][c1] = dust;
-            }
+        for (int i = 0; i < air; i++) A[i][C - 1] = A[i + 1][C - 1];
 
-            t *= -1;
+        for (int i = C - 1; i > 1; i--) A[air][i] = A[air][i - 1];
 
-            for (int i = 0; i < printY; i++) {
-                int dust = map[r1][c1];
-                r1 += t;
+        A[air][1] = 0;
 
-                if (dust == 0) continue;
+        // 시계
+        ans -= A[air + 2][0];
 
-                if (map[r1][c1] != -1) tmp[r1][c1] = dust;
-            }
-        }
+        A[air + 2][0] = 0;
 
-        int r2 = r[1], c2 = c[1];
-        printY = R - r2;
-        printX = C - 1;
-        t = 1;
+        for (int i = air + 2; i < R - 1; i++) A[i][0] = A[i + 1][0];
 
-        if (r2 != R - 1) {
-            for (int k = 0; k < 2; k++) {
-                for (int i = 0; i < printX; i++) {
-                    int dust = map[r2][c2];
-                    c2 += t;
+        for (int i = 0; i < C - 1; i++) A[R - 1][i] = A[R - 1][i + 1];
 
-                    if (dust == -1 || dust == 0) continue;
+        for (int i = R - 1; i > air + 1; i--) A[i][C - 1] = A[i - 1][C - 1];
 
-                    tmp[r2][c2] = dust;
-                }
+        for (int i = C - 1; i > 1; i--) A[air + 1][i] = A[air + 1][i - 1];
 
-                for (int i = 0; i < printY - 1; i++) {
-                    int dust = map[r2][c2];
-                    r2 += t;
-
-                    if (dust == 0) continue;
-
-                    if (map[r2][c2] != -1) tmp[r2][c2] = dust;
-                }
-
-                t *= -1;
-            }
-        }
-        copy();
-    }
-
-    static void copy() {
-        int r1 = r[0], c1 = c[0];
-        int printX = C - 1, printY = r1, t = 1;
-
-        for (int k = 0; k < 2; k++) {
-            for (int i = 0; i < printX; i++) {
-                if (map[r1][c1] != -1) map[r1][c1] = tmp[r1][c1];
-                c1 += t;
-            }
-
-            t *= -1;
-
-            for (int i = 0; i < printY; i++) {
-                if (map[r1][c1] != -1) map[r1][c1] = tmp[r1][c1];
-                r1 += t;
-            }
-        }
-
-        int r2 = r[1], c2 = c[1];
-        printY = R - r2;
-        printX = C - 1;
-        t = 1;
-
-        if (r2 != R - 1) {
-            for (int k = 0; k < 2; k++) {
-                for (int i = 0; i < printX; i++) {
-                    if (map[r2][c2] != -1) map[r2][c2] = tmp[r2][c2];
-                    c2 += t;
-                }
-
-                for (int i = 0; i < printY - 1; i++) {
-                    if (map[r2][c2] != -1) map[r2][c2] = tmp[r2][c2];
-                    r2 += t;
-                }
-
-                t *= -1;
-            }
-        }
+        A[air + 1][1] = 0;
     }
 
     static boolean inRange(int y, int x) {
-        return (y >= 0 && y < R) && (x >= 0 && x < C);
+        return (0 <= y && y < R) && (0 <= x && x < C);
     }
 }
