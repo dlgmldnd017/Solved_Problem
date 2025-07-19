527782cd823fd622777e27c36c3673b62e43d0ea
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-07-19 22:34:31 +0900

    [Gold IV] Title: 키 순서, Time: 396 ms, Memory: 36008 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static final int INF = 500;
+
     static int N, M, ans;
-    static List<Integer>[] fwList, bwList;
-    static boolean[] visited;
+    static int[][] dist;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,56 +17,45 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        fwList = new ArrayList[N + 1];
-        bwList = new ArrayList[N + 1];
-
-        visited = new boolean[N + 1];
-
+        dist = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
-            fwList[i] = new ArrayList<>();
-            bwList[i] = new ArrayList<>();
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            fwList[a].add(b);
-            bwList[b].add(a);
+            dist[a][b] = 1;
         }
 
-        for (int i = 1; i <= N; i++) {
-            int biggerCnt = solve(fwList, i);
-            int smallerCnt = solve(bwList, i);
-
-            if(biggerCnt + smallerCnt == N - 1) ans++;
-        }
+        solve();
 
         System.out.println(ans);
     }
 
-    static int solve(List<Integer>[] list, int start) {
-        int cnt = 0;
+    static void solve() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                if (dist[i][k] == INF) continue;
 
-        Arrays.fill(visited, false);
-        visited[start] = true;
-
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-
-            for (int next : list[cur]) {
-                if (visited[next]) continue;
-
-                cnt++;
-                q.add(next);
-                visited[next] = true;
+                for (int j = 1; j <= N; j++) {
+                    if (dist[k][j] == INF) continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
             }
         }
 
-        return cnt;
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+            
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (dist[i][j] < INF || dist[j][i] < INF) cnt++;
+            }
+            
+            if (cnt == N - 1) ans++;
+        }
     }
 }
