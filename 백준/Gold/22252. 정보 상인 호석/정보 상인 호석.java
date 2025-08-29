a86d766fa4c1f99bdd27550199e72451253a062a
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-08-29 16:10:23 +0900

    [Gold V] Title: 정보 상인 호석, Time: 996 ms, Memory: 146596 KB -BaekjoonHub

--- 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.java
+++ 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int idx;
     static long ans;
-    static Map<String, PriorityQueue<Long>> map = new HashMap<>();
+    static Map<String, Integer> map = new HashMap<>();
+    static List<PriorityQueue<Long>> list = new ArrayList<>();
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,19 +19,31 @@ public class Main {
             int command = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
 
-            PriorityQueue<Long> pq = map.computeIfAbsent(name, k -> new PriorityQueue<>());
-
+            int pos = -1;
             switch (command) {
                 case 1:
-                    int k = Integer.parseInt(st.nextToken());
-                    for (int i = 0; i < k; i++) {
-                        pq.add(-Long.parseLong(st.nextToken()));
+                    if (!map.containsKey(name)) {
+                        pos = idx++;
+                        map.put(name, pos);
+                        list.add(new PriorityQueue<>());
+                    } else {
+                        pos = map.get(name);
                     }
+
+                    int k = Integer.parseInt(st.nextToken());
+
+                    for (int i = 0; i < k; i++) {
+                        list.get(pos).add(-Long.parseLong(st.nextToken()));
+                    }
+
                     break;
 
                 case 2:
                     if (map.containsKey(name)) {
                         int b = Integer.parseInt(st.nextToken());
+                        pos = map.get(name);
+
+                        PriorityQueue<Long> pq = list.get(pos);
                         while (!pq.isEmpty() && b-- > 0) {
                             ans += (-pq.poll());
                         }
