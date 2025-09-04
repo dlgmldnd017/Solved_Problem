dde1bf13e5a8fda68d35928088dc6a0950fb893c
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-09-04 23:38:52 +0900

    [level 1] Title: 폰켓몬, Time: 10.32 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,25 +1,11 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        
-        int len = nums.length;
-        
-        Set<Integer> set = new HashSet<>();
-        
-        for (int i = 0; i < len; i++) {
-            set.add(nums[i]);
-        }
-        
-        len /= 2;
-        
-        if (set.size() < len) {
-            answer = set.size();
-        } else {
-            answer = len;
-        }
-        
-        return answer;
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
     }
 }
