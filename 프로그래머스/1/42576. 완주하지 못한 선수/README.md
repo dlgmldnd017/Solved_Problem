c97d348f75c73d71e11cc351985af0cbbf3f45dc
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-09-05 00:05:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.29 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 95 MB, 시간: 75.83 ms
+메모리: 96.9 MB, 시간: 83.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 20:32:20
+2025년 09월 05일 00:05:05
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,30 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
+
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<completion.length; i++){
-            map.put(completion[i], map.getOrDefault(completion[i], 0)+1);
+        
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
-        for(int i=0; i<participant.length; i++){
-            if(map.containsKey(participant[i])) {
-                int cnt = map.get(participant[i])-1;
-                
-                if(cnt == 0) {
-                    map.remove(participant[i]);
-                }
-                else {
-                    map.put(participant[i], cnt);
-                }
-                
-            }
-            else {
-                answer = participant[i];
-                break;
-            }
+        for (int i = 0; i < completion.length; i++) {
+            int cnt = map.get(completion[i]) - 1;
+            if (cnt == 0) map.remove(completion[i]);
+            else map.put(completion[i], cnt);
         }
-        
-        return answer;
+
+        return map.keySet().iterator().next();
     }
 }
