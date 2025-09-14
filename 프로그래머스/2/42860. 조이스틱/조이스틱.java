d7f4954d09b8ba88c2eb2cab7e6d0a035df69e8a
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-09-14 22:11:23 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,27 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(String name) {
-        int answer = 0;
-        int length = name.length();
+        int len = name.length();
         
-        // 각 자리에서 알파벳 변경 비용 계산
-        for (int i = 0; i < length; i++) {
-            char target = name.charAt(i);
-            answer += Math.min(target - 'A', 'Z' - target + 1);  // ▲ 또는 ▼ 조작 최소값
+        int vertical = 0;
+        
+        for (int i = 0; i < len; i++) {
+            char c = name.charAt(i);
+            vertical += Math.min(c - 'A', 'Z' - c + 1);
+        }
+        
+        int horizontal = len - 1;
+        
+        int i = 0;
+        while (i < len) {
+            int next = i + 1;
+            while (next < len && name.charAt(next) == 'A') next++;
+            
+            int tmp = (len - next);
+            horizontal = Math.min(horizontal, (i << 1) + tmp);
+            horizontal = Math.min(horizontal, (tmp << 1) + i);
+            
+            i = next;
         }
 
-        // 커서 이동 최소화
-        int minMove = length - 1;  // 최대한 오른쪽으로 가는 경우
-        for (int i = 0; i < length; i++) {
-            int next = i + 1;
-            // 연속된 A 처리
-            while (next < length && name.charAt(next) == 'A') {
-                next++;
-            }
-            // 오른쪽으로 갔다가 왼쪽으로 돌아오는 경우와 왼쪽으로 갔다가 오른쪽으로 돌아오는 경우 비교
-            minMove = Math.min(minMove, i + length - next + Math.min(i, length - next));
-        }
-        
-        answer += minMove;
-        return answer;
+        return vertical + horizontal;
     }
 }
