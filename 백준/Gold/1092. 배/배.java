dd738b2f25fac76360a253f53e13c56378e0994f
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-09-21 22:09:32 +0900

    [Gold V] Title: 배, Time: 200 ms, Memory: 15112 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -4,48 +4,54 @@ import java.util.*;
 
 public class Main {
     static int N, M, ans;
-    static List<Integer> crane = new ArrayList<>(), box = new ArrayList<>();
+    static int[] crane;
+    static List<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
 
+        crane = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) crane.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            crane[i] = Integer.parseInt(st.nextToken());
+        }
 
-        Collections.sort(crane);
-        Collections.reverse(crane);
+        Arrays.sort(crane);
 
         M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) box.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < M; i++) {
+            list.add(-Integer.parseInt(st.nextToken()));
+        }
 
-        Collections.sort(box);
-        Collections.reverse(box);
+        Collections.sort(list);
 
-        if (crane.get(0) < box.get(0)) ans = -1;
-        else solve();
+        if (crane[N - 1] < -list.get(0)) {
+            ans = -1;
+        } else {
+            solve();
+        }
 
         System.out.println(ans);
     }
 
     static void solve() {
-        while (!box.isEmpty()) {
-            int i = 0, j = 0;
+        while (!list.isEmpty()) {
+            int i = N - 1, j = 0;
 
-            while(j < N) {
-                if (i == box.size()) break;
-
-                if (box.get(i) <= crane.get(j)) {
-                    box.remove(i);
-                    j++;
+            while (i >= 0 && j < list.size()) {
+                if (crane[i] >= -list.get(j)) {
+                    list.remove(j);
+                    i--;
                 } else {
-                    i++;
+                    j++;
                 }
             }
+
             ans++;
         }
     }
