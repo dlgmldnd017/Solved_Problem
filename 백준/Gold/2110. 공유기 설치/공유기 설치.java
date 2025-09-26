0a7cc992fcdeb4645547ca36e0c2c95fa5120c1a
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-09-26 12:26:29 +0900

    [Gold IV] Title: 공유기 설치, Time: 244 ms, Memory: 28268 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, C, ans;
-    static List<Integer> list = new ArrayList<>();
+    static int[] arr;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,39 +15,44 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
+        arr = new int[N];
+
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Collections.sort(list);
+        Arrays.sort(arr);
 
-        ans = solve();
+        solve();
 
         System.out.println(ans);
     }
 
-    static int solve() {
-        int low = 1, high = list.get(list.size() - 1);
+    static void solve() {
+        int l = 1, r = arr[N - 1];
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while (l <= r) {
+            int mid = (l + r) >>> 1;
 
-            if (getInstallRouter(mid) < C) high = mid - 1;
-            else low = mid + 1;
+            if (C > check(mid)) {
+                r = mid - 1;
+            } else {
+                ans = mid;
+                l = mid + 1;
+            }
         }
-
-        return low - 1;
     }
 
-    static int getInstallRouter(int mid) {
-        int start = 0, cnt = 1;
+    static int check(int mid) {
+        int cnt = 1, prev = 0;
 
         for (int i = 1; i < N; i++) {
-            if (list.get(i) - list.get(start) >= mid) {
-                start = i;
+            if (arr[i] - arr[prev] >= mid) {
+                prev = i;
                 cnt++;
             }
         }
+
         return cnt;
     }
 }
