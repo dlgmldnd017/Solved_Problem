b44292ab4dd6fd3bb10e10dc2b8f4bece002ef15
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2025-09-26 17:05:33 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 92 ms, Memory: 13600 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,30 +1,17 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
-    int y, x, key, cnt;
+public class Main {
+    static int N, M, ans;
+    static char[][] ch;
 
-    public Node(int y, int x, int key, int cnt){
-        this.y = y;
-        this.x = x;
-        this.key = key;
-        this.cnt = cnt;
-    }
+    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1, 1, 0, 0};
 
-    public int compareTo(Node n){
-        return this.cnt - n.cnt;
-    }
-}
-
-public class Main{
-    static int N, M, startY, startX, ans;
-    static char map[][];
-    static boolean visited[][][];
-
-    static int dy[] = {0, 0, -1, 1};
-    static int dx[] = {-1, 1, 0, 0};
-
-    public static void main(String args[]) throws Exception{
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -32,80 +19,76 @@ public class Main{
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new char[N][M];
+        ch = new char[N][M];
 
-        visited = new boolean[64][N][M];
+        int startY = -1, startX = -1;
 
-        for(int i=0; i<N; i++){
-            String str = br.readLine();
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
 
-            for(int j=0; j<M; j++){
-                map[i][j] = str.charAt(j);
+            for (int j = 0; j < M; j++) {
+                ch[i][j] = input.charAt(j);
 
-                if(map[i][j] == '0'){
+                if (ch[i][j] == '0') {
                     startY = i;
                     startX = j;
                 }
             }
         }
 
-        ans = Integer.MAX_VALUE;
+        ans = -1;
 
-        solve();
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(startY, startX, 0, 0));
 
-        if(ans == Integer.MAX_VALUE) ans = -1;
+        boolean[][][] visited = new boolean[1 << 5 + 1][N][M];
+        visited[0][startY][startX] = true;
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (ch[cur.y][cur.x] == '1') {
+                ans = cur.cnt;
+                break;
+            }
+
+            for (int k = 0; k < 4; k++) {
+                int ny = cur.y + dy[k];
+                int nx = cur.x + dx[k];
+
+                if (!inRange(ny, nx) || ch[ny][nx] == '#' || visited[cur.key][ny][nx]) continue;
+
+                if ('A' <= ch[ny][nx] && ch[ny][nx] <= 'F') {
+                    if ((cur.key & (1 << ch[ny][nx] - 'A')) != 0) {
+                        visited[cur.key][ny][nx] = true;
+                        q.add(new Node(ny, nx, cur.cnt + 1, cur.key));
+                    }
+                } else if ('a' <= ch[ny][nx] && ch[ny][nx] <= 'f') {
+                    int newKey = cur.key | (1 << ch[ny][nx] - 'a');
+                    visited[newKey][ny][nx] = true;
+                    q.add(new Node(ny, nx, cur.cnt + 1, newKey));
+                } else {
+                    visited[cur.key][ny][nx] = true;
+                    q.add(new Node(ny, nx, cur.cnt + 1, cur.key));
+                }
+            }
+        }
 
         System.out.println(ans);
     }
 
-    static void solve(){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(startY, startX, 0, 0));
-        visited[0][startY][startX] = true;
-
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-
-            for(int k=0; k<4; k++){
-                int ny = dy[k] + cur.y;
-                int nx = dx[k] + cur.x;
-
-                // 범위를 벗어나거나, 해당 키를 들고 방문했거나, 벽인 경우
-                if(!inRange(ny, nx) || visited[cur.key][ny][nx] || map[ny][nx] == '#') continue;
-
-                // (1) 출구 일 경우
-                if(map[ny][nx] == '1'){
-                    ans = cur.cnt+1;
-                    return;
-                }
-
-                // (2) 열쇠일 경우
-                if('a' <= map[ny][nx] && map[ny][nx] <= 'f'){
-                    int key = cur.key | (1 << (map[ny][nx] - 'a'));
-                    
-                    visited[key][ny][nx] = true;
-                    pq.add(new Node(ny, nx, key, cur.cnt+1));
-                }
-
-                // (3) 문 일 경우
-                else if('A' <= map[ny][nx] && map[ny][nx] <= 'F'){
-                    // 지나갈 수 있다면
-                    if((cur.key & (1 << (map[ny][nx] - 'A'))) > 0){
-                        pq.add(new Node(ny, nx, cur.key, cur.cnt+1));
-                    }
-                }
-
-                // (4) 빈칸일 경우
-                else{
-                    pq.add(new Node(ny, nx, cur.key, cur.cnt+1));
-                }
-
-                visited[cur.key][ny][nx] = true;
-            }
-        }
+    static boolean inRange(int y, int x) {
+        return (0 <= y && y < N) && (0 <= x && x < M);
     }
 
-    static boolean inRange(int y, int x){
-        return (y>=0&&y<N) && (x>=0&&x<M);
+    static class Node {
+        int y, x, cnt, key;
+
+        Node(int y, int x, int cnt, int key) {
+            this.y = y;
+            this.x = x;
+            this.cnt = cnt;
+            this.key = key;
+        }
     }
 }
