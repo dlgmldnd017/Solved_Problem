1ab1dab966efa50f1189fd3a1f0f13a20c769f00
HUIUNG-LEE <42001306+dlgmldnd017@users.noreply.github.com>
2026-02-03 22:45:25 +0900

    [Gold I] Title: 외판원 순회, Time: 116 ms, Memory: 17212 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -3,44 +3,51 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static int N, map[][], dp[][], ans;
+    static final int INF = 17_000_000;
+
+    static int N, M;
+    static int[][] arr, dp;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
+        M = (1 << N) - 1;
 
-        map = new int[N][N];
+        arr = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        dp = new int[N][(1 << N) - 1];
+        dp = new int[N][M];
         for (int i = 0; i < N; i++) Arrays.fill(dp[i], -1);
 
-        ans = solve(0, 1);
-
-        System.out.println(ans);
+        System.out.print(solve(0, 1));
     }
 
     static int solve(int cur, int visit) {
-        if (visit == (1 << N) - 1) {
-            if (map[cur][0] == 0) return 16_000_000;
-            return map[cur][0];
+        if (visit == M) {
+            if (arr[cur][0] == 0) return INF;
+            return arr[cur][0];
         }
 
         if (dp[cur][visit] != -1) return dp[cur][visit];
-        dp[cur][visit] = 16_000_000;
+        dp[cur][visit] = INF;
 
         for (int i = 1; i < N; i++) {
-            if ((visit & (1 << i)) == 0 && map[cur][i] != 0) {
-                dp[cur][visit] = Math.min(solve(i, visit | (1 << i)) + map[cur][i], dp[cur][visit]);
-            }
+            if (arr[cur][i] == 0) continue;
+
+            int next = 1 << i;
+            if ((visit & next) != 0) continue;
+
+            dp[cur][visit] = Math.min(solve(i, visit | next) + arr[cur][i], dp[cur][visit]);
         }
 
         return dp[cur][visit];
